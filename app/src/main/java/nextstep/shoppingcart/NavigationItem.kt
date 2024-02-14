f33255bfb56726137fcb22c8a81e545e5666ac70
malibinYun <nightmare73@naver.com>
2024-02-14 21:53:15 +0900

    껍데기 composables 추가

---
+++ app/src/main/java/nextstep/shoppingcart/NavigationItem.kt
@@ -0,0 +1,127 @@
+package nextstep.shoppingcart
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+sealed class NavigationItem(val route: String) {
+
+    data object Products : NavigationItem("Products")
+    data object ProductDetail : NavigationItem("ProductDetail")
+    data object Cart : NavigationItem("Cart")
+}
+
+@Composable
+fun AppNavHost(
+    navHostController: NavHostController,
+    startDestination: String = NavigationItem.Products.route,
+) {
+    NavHost(
+        navController = navHostController,
+        startDestination = startDestination,
+    ) {
+        composable(NavigationItem.Products.route) {
+            ProductsScreen(navHostController)
+        }
+
+        composable(NavigationItem.ProductDetail.route) {
+            ProductDetailScreen(navHostController)
+        }
+
+        composable(NavigationItem.Cart.route) {
+            CartScreen(navHostController)
+        }
+
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductsScreen(navHostController: NavHostController) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(text = "상품 목록") },
+                actions = {
+                    IconButton(
+                        onClick = {
+                            navHostController.navigate(NavigationItem.Cart.route)
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Default.ShoppingCart,
+                            contentDescription = "뒤로가기",
+                        )
+                    }
+                })
+        }
+    ) { innerPadding ->
+        Text(
+            modifier = Modifier.padding(innerPadding),
+            text = "Products",
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductDetailScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "상품 상세") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로가기"
+                        )
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        Text(
+            text = "ProductDetailScreen",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CartScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "장바구니") },
+                navigationIcon = {
+                    IconButton(onClick = { navController.popBackStack() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "뒤로가기"
+                        )
+                    }
+                },
+            )
+        }
+    ) { innerPadding ->
+        Text(
+            text = "CartScreen",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
