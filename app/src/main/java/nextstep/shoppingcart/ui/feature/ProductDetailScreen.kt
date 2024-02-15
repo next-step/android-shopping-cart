2d94405946e4b262cb06160843e4372712e69535
namjackson <skawo32167@naver.com>
2024-02-15 15:44:29 +0900

    Step1 : Screen 추가 (상품목록,상품상세,카트)

---
+++ app/src/main/java/nextstep/shoppingcart/ui/feature/ProductDetailScreen.kt
@@ -0,0 +1,9 @@
+package nextstep.shoppingcart.ui.feature
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductDetailScreen() {
+    Text(text = "상품 상세화면")
+}
---
+++ app/src/main/java/nextstep/shoppingcart/ui/feature/ProductListScreen.kt
@@ -0,0 +1,9 @@
+package nextstep.shoppingcart.ui.feature
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProductListScreen() {
+    Text("상품리스트")
+}
