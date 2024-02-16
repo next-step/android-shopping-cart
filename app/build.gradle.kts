979373a27f869b24c13518e3f6c531348ec03a18
laco-dev <laco2951@gmail.com>
2024-02-16 13:25:28 +0900

    coil 이미지 라이브러리 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,4 +68,6 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     androidTestImplementation("androidx.navigation:navigation-testing:2.7.7")
+
+    implementation("io.coil-kt:coil-compose:2.5.0")
 }
