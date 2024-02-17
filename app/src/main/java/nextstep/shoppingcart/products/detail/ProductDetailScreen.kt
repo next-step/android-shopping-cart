f0bf22f3b3411f36109dfbebd7a7fc2843a32080
malibinYun <nightmare73@naver.com>
2024-02-17 23:50:55 +0900

    상품 상세 페이지 TopBar 컴포넌트 분리

--- app/src/main/java/nextstep/shoppingcart/products/detail/ProductDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/products/detail/ProductDetailScreen.kt
@@ -1,34 +1,18 @@
 package nextstep.shoppingcart.products.detail
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import nextstep.shoppingcart.products.detail.component.ProductDetailScreenTopBar
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductDetailScreen(navController: NavHostController) {
     Scaffold(
         topBar = {
-            TopAppBar(
-                title = { Text(text = "상품 상세") },
-                navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(
-                            imageVector = Icons.Filled.ArrowBack,
-                            contentDescription = "뒤로가기"
-                        )
-                    }
-                },
-            )
+            ProductDetailScreenTopBar(navController)
         }
     ) { innerPadding ->
         Text(
