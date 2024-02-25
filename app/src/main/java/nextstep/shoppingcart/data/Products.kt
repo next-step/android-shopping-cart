880b77d63551e4f62153ef8d7f30a7ef84d84ad4
namjackson <skawo32167@naver.com>
2024-02-25 16:49:02 +0900

    Step2 : Product, Products Mock데이터 추가

---
+++ app/src/main/java/nextstep/shoppingcart/data/Products.kt
@@ -0,0 +1,174 @@
+package nextstep.shoppingcart.data
+
+import nextstep.shoppingcart.domain.Product
+
+val Products: List<Product> = listOf(
+    Product(
+        id = 1,
+        name = "맛있는 Cupcake",
+        imageUrl = "https://source.unsplash.com/pGM4sjt_BdQ",
+        price = 2990
+    ),
+    Product(
+        id = 2,
+        name = "먹고 싶은 Donut",
+        imageUrl = "https://source.unsplash.com/Yc5sL-ejk6U",
+        price = 2990
+    ),
+    Product(
+        id = 3,
+        name = "존맛탱 Eclair",
+        imageUrl = "https://source.unsplash.com/-LojFX9NfPY",
+        price = 2990
+    ),
+    Product(
+        id = 4,
+        name = "테스트 Froyo",
+        imageUrl = "https://source.unsplash.com/3U2V5WqK1PQ",
+        price = 2990
+    ),
+    Product(
+        id = 5,
+        name = "Gingerbread",
+        imageUrl = "https://source.unsplash.com/Y4YR9OjdIMk",
+        price = 4990
+    ),
+    Product(
+        id = 6,
+        name = "Honeycomb",
+        imageUrl = "https://source.unsplash.com/bELvIg_KZGU",
+        price = 2990
+    ),
+    Product(
+        id = 7,
+        name = "Ice Cream Sandwich",
+        imageUrl = "https://source.unsplash.com/YgYJsFDd4AU",
+        price = 12990
+    ),
+    Product(
+        id = 8,
+        name = "Jellybean",
+        imageUrl = "https://source.unsplash.com/0u_vbeOkMpk",
+        price = 2990
+    ),
+    Product(
+        id = 9,
+        name = "KitKat",
+        imageUrl = "https://source.unsplash.com/yb16pT5F_jE",
+        price = 54900
+    ),
+    Product(
+        id = 10,
+        name = "Lollipop",
+        imageUrl = "https://source.unsplash.com/AHF_ZktTL6Q",
+        price = 29900
+    ),
+    Product(
+        id = 11,
+        name = "Marshmallow",
+        imageUrl = "https://source.unsplash.com/rqFm0IgMVYY",
+        price = 29900
+    ),
+    Product(
+        id = 12,
+        name = "Nougat",
+        imageUrl = "https://source.unsplash.com/qRE_OpbVPR8",
+        price = 29900
+    ),
+    Product(
+        id = 13,
+        name = "Oreo",
+        imageUrl = "https://source.unsplash.com/33fWPnyN6tU",
+        price = 29900
+    ),
+    Product(
+        id = 14,
+        name = "Pie",
+        imageUrl = "https://source.unsplash.com/aX_ljOOyWJY",
+        price = 29900
+    ),
+    Product(
+        id = 15,
+        name = "Chips",
+        imageUrl = "https://source.unsplash.com/UsSdMZ78Q3E",
+        price = 29900
+    ),
+    Product(
+        id = 16,
+        name = "Pretzels",
+        imageUrl = "https://source.unsplash.com/7meCnGCJ5Ms",
+        price = 29900
+    ),
+    Product(
+        id = 17,
+        name = "Smoothies",
+        imageUrl = "https://source.unsplash.com/m741tj4Cz7M",
+        price = 29900
+    ),
+    Product(
+        id = 18,
+        name = "Popcorn",
+        imageUrl = "https://source.unsplash.com/iuwMdNq0-s4",
+        price = 29900
+    ),
+    Product(
+        id = 19,
+        name = "Almonds",
+        imageUrl = "https://source.unsplash.com/qgWWQU1SzqM",
+        price = 2990
+    ),
+    Product(
+        id = 20,
+        name = "Cheese",
+        imageUrl = "https://source.unsplash.com/9MzCd76xLGk",
+        price = 2990
+    ),
+    Product(
+        id = 21,
+        name = "Apples",
+        imageUrl = "https://source.unsplash.com/1d9xXWMtQzQ",
+        price = 2990
+    ),
+    Product(
+        id = 22,
+        name = "Apple sauce",
+        imageUrl = "https://source.unsplash.com/wZxpOw84QTU",
+        price = 2990
+    ),
+    Product(
+        id = 23,
+        name = "Apple chips",
+        imageUrl = "https://source.unsplash.com/okzeRxm_GPo",
+        price = 2990
+    ),
+    Product(
+        id = 24,
+        name = "Apple juice",
+        imageUrl = "https://source.unsplash.com/l7imGdupuhU",
+        price = 2990
+    ),
+    Product(
+        id = 25,
+        name = "Apple pie",
+        imageUrl = "https://source.unsplash.com/bkXzABDt08Q",
+        price = 2990
+    ),
+    Product(
+        id = 26,
+        name = "Grapes",
+        imageUrl = "https://source.unsplash.com/y2MeW00BdBo",
+        price = 2990
+    ),
+    Product(
+        id = 27,
+        name = "Kiwi",
+        imageUrl = "https://source.unsplash.com/1oMGgHn-M8k",
+        price = 2990
+    ),
+    Product(
+        id = 28,
+        name = "Mango",
+        imageUrl = "https://source.unsplash.com/TIGDsyy0TK4",
+        price = 2990
+    )
+)
---
+++ app/src/main/java/nextstep/shoppingcart/domain/Product.kt
@@ -0,0 +1,8 @@
+package nextstep.shoppingcart.domain
+
+data class Product(
+    val id: Int,
+    val name: String,
+    val price: Int,
+    val imageUrl: String,
+)
