4a00ff9f0e8a343a6f703b98fde0b34edddd1ebe
강현석 <kekemusa37@gmail.com>
2024-03-07 22:29:20 +0900

    [분리] CountIndicator

--- app/src/main/java/nextstep/shoppingcart/ui/screen/cart/CartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/cart/CartScreen.kt
@@ -9,20 +9,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -41,15 +37,15 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.model.Product
+import nextstep.shoppingcart.ui.component.BottomText
+import nextstep.shoppingcart.ui.component.CountIndicator
 import nextstep.shoppingcart.ui.component.PriceText
 import nextstep.shoppingcart.ui.component.ProductImage
-import nextstep.shoppingcart.ui.component.BottomText
 import nextstep.shoppingcart.ui.component.ProductTitle
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -171,50 +167,6 @@ private fun CartItem(
             }
         }
         Spacer(modifier = Modifier.weight(weight = 1f))
-    }
-}
-
-@Composable
-private fun CountIndicator(
-    count: String,
-    onClickInc: () -> Unit,
-    onClickDec: () -> Unit
-) {
-    Row(
-        modifier = Modifier.width(width = 126.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Image(
-            modifier = Modifier
-                .weight(weight = 1f)
-                .fillMaxHeight()
-                .clickable {
-                    onClickDec()
-                },
-            imageVector = Icons.Filled.KeyboardArrowDown,
-            contentDescription = stringResource(R.string.minus)
-        )
-        Text(
-            modifier = Modifier
-                .weight(weight = 1f)
-                .fillMaxHeight()
-                .semantics {
-                    contentDescription = "Count"
-                },
-            text = count,
-            textAlign = TextAlign.Center,
-            fontSize = 22.sp
-        )
-        Image(
-            modifier = Modifier
-                .weight(weight = 1f)
-                .fillMaxHeight()
-                .clickable {
-                    onClickInc()
-                },
-            imageVector = Icons.Filled.KeyboardArrowUp,
-            contentDescription = stringResource(R.string.plus)
-        )
     }
 }
 
