3604501b018aaeeb1d1bbeae34692f1c0964ef12
kyudong3 <kyudong3@gmail.com>
2024-08-15 00:04:46 +0900

    feat: 첫 코드 정리 및 목록 화면 Composable 추가

---
+++ app/src/main/java/nextstep/shoppingcart/ui/shopping/productlist/ProductList.kt
@@ -0,0 +1,13 @@
+package nextstep.shoppingcart.ui.shopping.productlist
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ProductList(modifier: Modifier = Modifier) {
+    Scaffold { paddingValues ->
+
+    }
+}
