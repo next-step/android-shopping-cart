383fdf43940c14721d9637f3219ce16d4f56b9a2
1002533 <bf.winfive@sk.com>
2024-08-15 08:54:41 +0900

    feat: 상품목록 타이틀을 구성

---
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductListScreen.kt
@@ -0,0 +1,20 @@
+package nextstep.shoppingcart.ui.screen
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ProductList(modifier: Modifier = Modifier) {
+    Scaffold (
+        topBar = {
+            Title(title = "상품 목록") {
+
+            }
+        },
+        content = { paddingValues ->  
+            
+        }
+    )
+}
