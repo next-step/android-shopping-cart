6f8866bde5763602b8133743b777f27649102d15
jay.lee <stkment@gmail.com>
2024-08-16 03:00:48 +0900

    feat: ProductDetail Screen 상세 구현

--- README.md
+++ README.md
@@ -1,20 +1,27 @@
 # android-shopping-cart
 
 ## 🚀 1단계 - 장바구니(상품 목록)
+
 - [v] Product 모델 생성
 - [v] ProductCard 컴포넌트 생성
-  - [v] ProductCard 컴퍼넌트 상세 구현
-  - [v] ProductCard 컴퍼넌트 프리뷰 추가
+    - [v] ProductCard 컴퍼넌트 상세 구현
+    - [v] ProductCard 컴퍼넌트 프리뷰 추가
 - [v] ProductList Screen 생성
-  - [v] ProductList Screen 상세 구현
-  - [v] ProductList Screen 프리뷰 추가
+    - [v] ProductList Screen 상세 구현
+    - [v] ProductList Screen 프리뷰 추가
 
 ## 🚀 2단계 - 장바구니(상품 상세)
-- [] 상품 상세 화면 Navigation 구현
-  - [v] Compose Navigation component 추가
-  - [v] 상품 목록에서 상품을 누르면 상품 상세 화면으로 이동
-  - [] 뒤로 가기 버튼이나 아이콘을 누르면 직전 화면으로 돌아감
-- [] 장바구니 화면(상세구현 x) 이동
-  - [] 상품 목록에서 장바구니 아이콘을 누르면 장바구니 화면으로 이동
-  - [] 상품 상세에서 장바구니 담기 버튼을 누르면 장바구니 화면으로 이동
-  - [] 뒤로 가기 버튼이나 아이콘을 누르면 직전 화면으로 돌아감
+
+- [v] 상품 상세 화면 Navigation 구현
+    - [v] Compose Navigation component 추가
+    - [v] 상품 목록에서 상품을 누르면 상품 상세 화면으로 이동
+    - [v] 뒤로 가기 버튼이나 아이콘을 누르면 직전 화면으로 돌아감
+- [] 상품 상세 화면 상세 구현
+    - [v] 상품 상세 화면 상세 구현
+    - [] 상품 상세 화면 상세 프리뷰 추가
+- [] 장바구니 화면 구현(상세 구현 x)
+    - [] 장바구니 빈 화면 구현
+- [] 장바구니 화면 이동
+    - [] 상품 목록에서 장바구니 아이콘을 누르면 장바구니 화면으로 이동
+    - [] 상품 상세에서 장바구니 담기 버튼을 누르면 장바구니 화면으로 이동
+    - [] 뒤로 가기 버튼이나 아이콘을 누르면 직전 화면으로 돌아감
--- app/src/main/java/nextstep/shoppingcart/ui/product/detail/ProductDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product/detail/ProductDetailScreen.kt
@@ -1,22 +1,42 @@
 package nextstep.shoppingcart.ui.product.detail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import coil.compose.AsyncImage
 import nextstep.shoppingcart.R
+import nextstep.shoppingcart.data.Products
+import nextstep.shoppingcart.data.ProductsImpl
+import nextstep.shoppingcart.domain.model.Product
+import nextstep.shoppingcart.ui.theme.Blue50
 
 @Composable
 internal fun ProductDetailRoute(
@@ -24,14 +44,23 @@ internal fun ProductDetailRoute(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController(),
 ) {
+    val products: Products = remember { ProductsImpl() }
+    val product = products.findById(productId) ?: return
+
     ProductDetailScreen(
+        product = product,
+        navigateUp = { navController.popBackStack() },
         modifier = modifier.fillMaxSize(),
     )
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-internal fun ProductDetailScreen(modifier: Modifier = Modifier) {
+internal fun ProductDetailScreen(
+    product: Product,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(
@@ -39,7 +68,7 @@ internal fun ProductDetailScreen(modifier: Modifier = Modifier) {
                     Text(text = stringResource(id = R.string.product_detail_toolbar_title))
                 },
                 navigationIcon = {
-                    IconButton(onClick = { /* TODO */ }) {
+                    IconButton(onClick = navigateUp) {
                         Icon(
                             imageVector = Icons.Filled.ArrowBack,
                             contentDescription = stringResource(id = R.string.navigate_back),
@@ -50,12 +79,94 @@ internal fun ProductDetailScreen(modifier: Modifier = Modifier) {
         },
         modifier = modifier,
     ) { paddingValues ->
-        Column(
+        ProductDetailContent(
+            product = product,
             modifier =
-                Modifier
+                modifier
                     .padding(paddingValues)
                     .fillMaxSize(),
+        )
+    }
+}
+
+@Composable
+private fun ProductDetailContent(
+    product: Product,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Column(
+            modifier = Modifier.weight(1f),
         ) {
+            AsyncImage(
+                model = product.imgUrl,
+                contentDescription =
+                    stringResource(
+                        id = R.string.product_image_content_description,
+                        product.name,
+                    ),
+                modifier = Modifier.fillMaxWidth(),
+            )
+            Text(
+                text = product.name,
+                overflow = TextOverflow.Ellipsis,
+                style =
+                    MaterialTheme.typography.titleLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                    ),
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 18.dp, vertical = 18.dp),
+            )
+            Divider(modifier = Modifier.height(1.dp))
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 18.dp, vertical = 18.dp),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.product_detail_price_title),
+                    style =
+                        MaterialTheme.typography.titleMedium.copy(
+                            fontSize = 20.sp,
+                        ),
+                )
+                Text(
+                    text = stringResource(id = R.string.product_price, product.price),
+                    style =
+                        MaterialTheme.typography.titleMedium.copy(
+                            fontSize = 20.sp,
+                        ),
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = Blue50,
+                    contentColor = Color.White,
+                ),
+            shape = RectangleShape,
+            modifier =
+                Modifier
+                    .fillMaxWidth()
+                    .height(54.dp),
+        ) {
+            Text(
+                text = stringResource(id = R.string.product_detail_btn_title),
+                style =
+                    MaterialTheme.typography.bodyMedium.copy(
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                    ),
+            )
         }
     }
 }
