41a092375fdd9c25ff91d97686396b5c38a1cb38
jay.lee <stkment@gmail.com>
2024-08-15 12:56:38 +0900

    feat: Icon component 교체

--- app/src/main/java/nextstep/shoppingcart/ui/product/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product/ProductListScreen.kt
@@ -1,6 +1,5 @@
 package nextstep.shoppingcart.ui.product
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,6 +11,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -52,7 +52,7 @@ internal fun ProductListScreen(
                 },
                 actions = {
                     IconButton(onClick = { /*TODO*/ }) {
-                        Image(
+                        Icon(
                             imageVector = Icons.Filled.ShoppingCart,
                             contentDescription =
                             stringResource(
