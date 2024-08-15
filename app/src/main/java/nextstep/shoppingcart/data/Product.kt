8119c6536ca10156dac89a0dd1bd9dcbeb187d07
minhyukseul <mioyo2@gmail.com>
2024-08-15 10:48:11 +0900

    feat: 상품 목록을 구성한다

---
+++ app/src/main/java/nextstep/shoppingcart/data/Product.kt
@@ -0,0 +1,8 @@
+package nextstep.shoppingcart.data
+
+data class Product(
+    val imageUrl: String,
+    val name: String,
+    val price: Int
+)
+
---
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductItemScreen.kt
@@ -0,0 +1,67 @@
+package nextstep.shoppingcart.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import nextstep.shoppingcart.data.Product
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    val product = Product(
+        imageUrl = "https://picsum.photos/156/158",
+        name = "상품 이름을 테스트해보겠습니다 말줄입이 되나요",
+        price = 1200000000
+    )
+    ProductItem(product)
+}
+
+@Composable
+fun ProductItem(product: Product) {
+    Column(
+        modifier = Modifier
+            .width(156.dp)
+            .height(198.dp)
+    ) {
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = null,
+            modifier = Modifier
+                .width(156.dp)
+                .height(158.dp)
+        )
+        Column(
+            modifier = Modifier
+                .padding(top = 4.dp)
+                .fillMaxSize()
+        ) {
+            Text(
+                text = product.name,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp,
+                color = Color(0xFF333333),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+            Text(
+                text = "${product.price}원",
+                fontSize = 16.sp,
+                color = Color(0xFF333333),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
