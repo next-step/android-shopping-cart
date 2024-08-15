df0082b9392c770833295478587de01037b5b749
MyStoryG <bookwriterseo@gmail.com>
2024-08-16 01:31:49 +0900

    docs: 구현 목록 작성

--- app/src/main/java/nextstep/shoppingcart/ProductsActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/ProductsActivity.kt
@@ -22,7 +22,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ShoppingCartTheme {
+                        Greeting("Android")
+                    }
                 }
             }
         }
