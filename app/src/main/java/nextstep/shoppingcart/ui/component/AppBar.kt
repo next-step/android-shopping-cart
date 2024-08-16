57d145456c308793c0ec106d2d749e384188b6b5
OYJ <oyj7677@gmail.com>
2024-08-17 03:37:32 +0900

    Rename : TopBar.kt -> AppBar.kt 변경
    1. 탑 바와 바텀 바 컴포넌트를 포함하는 이름으로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/component/AppBar.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/component/AppBar.kt
@@ -5,9 +5,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -63,4 +65,22 @@ fun ProductDetailTopBar(modifier: Modifier = Modifier) {
             }
         }
     )
+}
+
+@Composable
+fun ProductBottomBar(modifier: Modifier = Modifier) {
+    BottomAppBar(
+        modifier
+            .fillMaxWidth(),
+        containerColor = Color(0xFF2196F3)
+    ) {
+        Text(
+            text = stringResource(id = R.string.text_shopping_cart),
+            modifier = Modifier
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            color = Color.White,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
 }
