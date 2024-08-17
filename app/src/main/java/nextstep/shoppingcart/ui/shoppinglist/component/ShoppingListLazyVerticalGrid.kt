ea6b6784df1ae40f007ca68db5d428cd54c503e6
s9hn <tnghqn@naver.com>
2024-08-17 14:10:53 +0900

    feat: 장바구니 아이템 리스트 컴포넌트 구현

---
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppinglist/component/ShoppingListLazyVerticalGrid.kt
@@ -0,0 +1,52 @@
+package nextstep.shoppingcart.ui.shoppinglist.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.shoppingcart.R
+import nextstep.shoppingcart.ui.shoppinglist.model.Product
+import nextstep.shoppingcart.ui.shoppinglist.model.dummyProducts
+
+@Composable
+fun ShoppingListLazyVerticalGrid(
+    products: List<Product>,
+    modifier: Modifier = Modifier,
+) {
+    val shoppingListDescription = stringResource(R.string.shopping_list_description)
+
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(
+                horizontal = 20.dp,
+                vertical = 12.dp,
+            )
+            .semantics { contentDescription = shoppingListDescription },
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
+        items(
+            items = products,
+            key = { product -> product.id },
+        ) {
+            ShoppingListItem(product = it)
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ShoppingListPreview() {
+    ShoppingListLazyVerticalGrid(products = dummyProducts)
+}
