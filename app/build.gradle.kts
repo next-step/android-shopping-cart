c906c353182520586d7f403d7fc3750703ff4555
sim junbo <46774431+simuelunbo@users.noreply.github.com>
2024-08-17 20:22:52 +0900

    🚀 1단계 - 장바구니(상품 목록) (#27)
    * docs: README 요구사항 및 step1 구현 내용 작성
    
    * chore: manifest 인터넷 permission 추가
    
    * chore: coil 라이브러리 추가
    
    * feat: UI에 필요한 String Resource 추가
    
    * feat: Product Model 추가
    
    * chore: Material3 API 사용을 위한 컴파일러 옵션 추가
    
    * feat: Main AppBar UI 구현
    
    * feat: 상품 Item 컴포넌트 구현
    
    * feat: 상품 목록 MainScreen 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,6 +35,7 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
+        freeCompilerArgs += "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
     }
     buildFeatures {
         compose = true
@@ -58,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("io.coil-kt:coil-compose:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
