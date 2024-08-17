807b1b4b9b5b2150dee7245c9f2191fa85c0c625
OYJ <oyj7677@gmail.com>
2024-08-17 12:05:34 +0900

    Feat : 상품 정보 컴포넌트 분리
    1. 이미지 + 이름, 가격 분리

--- app/src/main/java/nextstep/shoppingcart/ui/screen/ProductDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductDetailScreen.kt
@@ -1,29 +1,20 @@
 package nextstep.shoppingcart.ui.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.ui.component.BasicTopBar
 import nextstep.shoppingcart.ui.component.ProductBottomBar
+import nextstep.shoppingcart.ui.component.ProductOverview
+import nextstep.shoppingcart.ui.component.ProductPrice
 import nextstep.shoppingcart.ui.data.Product
 
 @Composable
@@ -39,22 +30,7 @@ fun ProductDetailScreen(modifier: Modifier, product: Product) {
         Column(
             modifier = modifier.padding(innerPadding)
         ) {
-            AsyncImage(
-                modifier = Modifier.height(360.dp),
-                model = product.imgUrl,
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                placeholder = ColorPainter(Color.Black)
-            )
-
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(18.dp),
-                text = product.name,
-                style = MaterialTheme.typography.bodyLarge,
-                fontWeight = FontWeight.Bold,
-            )
+            ProductOverview(Modifier, product)
 
             Divider(
                 modifier = Modifier.fillMaxWidth(),
@@ -62,25 +38,12 @@ fun ProductDetailScreen(modifier: Modifier, product: Product) {
                 thickness = 1.dp
             )
 
-            Row(
-                modifier
-                    .padding(18.dp)
+            ProductPrice(
+                modifier = Modifier
                     .fillMaxWidth()
-            ) {
-                Text(
-                    text = stringResource(id = R.string.text_price),
-                    style = MaterialTheme.typography.titleSmall,
-                    textAlign = TextAlign.Start,
-                )
-
-                Spacer(modifier = Modifier.weight(1f))
-
-                Text(
-                    text = stringResource(id = R.string.formatted_price, product.price),
-                    style = MaterialTheme.typography.titleSmall,
-                    textAlign = TextAlign.End,
-                )
-            }
+                    .padding(18.dp),
+                product = product
+            )
         }
     }
 }
