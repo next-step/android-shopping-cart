125c3e918befd9b791aa1f0d79c558fe4db81284
Jihoi-Kang <jihoi.kang@gmail.com>
2024-08-18 12:22:45 +0900

    feat: implement product detail

--- app/src/main/java/nextstep/shoppingcart/ui/product/detail/ProductDetailActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product/detail/ProductDetailActivity.kt
@@ -1,17 +1,28 @@
 package nextstep.shoppingcart.ui.product.detail
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import nextstep.shoppingcart.ui.model.Product
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
+@Suppress("DEPRECATION")
 class ProductDetailActivity : ComponentActivity() {
+
+    private val product: Product by lazy {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent.getParcelableExtra(EXTRA_PRODUCT, Product::class.java)
+        } else {
+            intent.getParcelableExtra(EXTRA_PRODUCT)
+        } ?: throw IllegalArgumentException("product is required")
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingCartTheme {
-                ProductDetailScreen()
+                ProductDetailScreen(product)
             }
         }
     }
