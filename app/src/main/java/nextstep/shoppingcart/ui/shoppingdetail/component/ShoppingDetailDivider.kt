893902099952fa4a1d338811e8903e9b877fc327
s9hn <tnghqn@naver.com>
2024-08-19 21:48:30 +0900

    refactor: Divider 컴포넌트화 및 적용

---
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingdetail/component/ShoppingDetailDivider.kt
@@ -0,0 +1,27 @@
+package nextstep.shoppingcart.ui.shoppingdetail.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingDetailDivider(
+    modifier: Modifier = Modifier,
+) {
+    Divider(
+        modifier = modifier
+            .height(2.dp)
+            .background(Color.Gray),
+    )
+}
+
+@Preview
+@Composable
+private fun ShoppingDetailDividerPreview() {
+    ShoppingDetailDivider()
+}
