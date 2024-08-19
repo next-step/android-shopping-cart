3175c13ee65658daa72af62658f78c61587bf463
MyStoryG <bookwriterseo@gmail.com>
2024-08-20 01:09:50 +0900

    feat: 장바구니 화면에서 상품 목록 화면으로 이동 구현

--- app/src/main/java/nextstep/shoppingcart/view/CartTopAppBar.kt
+++ app/src/main/java/nextstep/shoppingcart/view/CartTopAppBar.kt
@@ -1,5 +1,6 @@
 package nextstep.shoppingcart.view
 
+import android.content.Intent
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,6 +9,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.R
@@ -16,12 +18,19 @@ import nextstep.shoppingcart.view.resource.ShoppingCartTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CartTopAppBar() {
+    val context = LocalContext.current
+
     TopAppBar(
         title = {
             Text(text = stringResource(id = R.string.cart_app_bar_title))
         },
         navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(
+                onClick = {
+                    val intent = Intent(context, ProductsActivity::class.java)
+                    context.startActivity(intent)
+                }
+            ) {
                 Icon(
                     Icons.Filled.ArrowBack,
                     contentDescription = stringResource(id = R.string.cart_app_bar_back_icon_description)
