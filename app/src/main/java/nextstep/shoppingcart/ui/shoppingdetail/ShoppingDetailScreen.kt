b7cb31f1071253cf2a7e8898e8bd2c4fee4e2c61
s9hn <tnghqn@naver.com>
2024-08-19 22:01:24 +0900

    refactor: ShoppingDetailProductTitle 컴포넌트화 및 적용

--- app/src/main/java/nextstep/shoppingcart/ui/shoppingdetail/ShoppingDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingdetail/ShoppingDetailScreen.kt
@@ -1,31 +1,25 @@
 package nextstep.shoppingcart.ui.shoppingdetail
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Divider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.ui.component.ShoppingProductImage
 import nextstep.shoppingcart.ui.component.ShoppingTopBar
+import nextstep.shoppingcart.ui.shoppingdetail.component.ShoppingDetailDivider
+import nextstep.shoppingcart.ui.shoppingdetail.component.ShoppingDetailProductAddButton
+import nextstep.shoppingcart.ui.shoppingdetail.component.ShoppingDetailProductPrice
+import nextstep.shoppingcart.ui.shoppingdetail.component.ShoppingDetailProductTitle
 import nextstep.shoppingcart.ui.shoppinglist.model.dummyProducts
-import nextstep.shoppingcart.ui.theme.RobotoBold
 
 @Composable
 fun ShoppingDetailScreen(
@@ -47,36 +41,11 @@ fun ShoppingDetailScreen(
                 .width(360.dp)
                 .height(360.dp),
         )
-        Text(
-            text = product.name,
-            color = Color.Black,
-            fontSize = 24.sp,
-            fontFamily = RobotoBold,
-            maxLines = 1,
-            modifier = Modifier.padding(18.dp),
-        )
-        Divider(
-            modifier = Modifier
-                .height(2.dp)
-                .background(Color.Gray),
-        )
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(18.dp),
-        ) {
-            Text(text = "금액")
-            Text(text = product.price.toString())
-        }
+        ShoppingDetailProductTitle(product = product)
+        ShoppingDetailDivider()
+        ShoppingDetailProductPrice(product = product)
         Spacer(modifier = Modifier.weight(1f))
-        Button(
-            onClick = { /*TODO*/ },
-            shape = RoundedCornerShape(0.dp),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text(text = "장바구니 담기")
-        }
+        ShoppingDetailProductAddButton({})
     }
 }
 
---
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingdetail/component/ShoppingDetailProductTitle.kt
@@ -0,0 +1,31 @@
+package nextstep.shoppingcart.ui.shoppingdetail.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.ui.shoppinglist.model.Product
+import nextstep.shoppingcart.ui.shoppinglist.model.dummyProducts
+import nextstep.shoppingcart.ui.theme.RobotoBold
+
+@Composable
+fun ShoppingDetailProductTitle(product: Product) {
+    Text(
+        text = product.name,
+        color = Color.Black,
+        fontSize = 24.sp,
+        fontFamily = RobotoBold,
+        maxLines = 1,
+        modifier = Modifier.padding(18.dp),
+    )
+}
+
+@Preview
+@Composable
+private fun ShoppingDetailProductTitlePreview() {
+    ShoppingDetailProductTitle(product = dummyProducts[0])
+}
