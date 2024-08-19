b121d6a5dd42b7d088774ae11b4090d9e95bc28c
jay.lee <stkment@gmail.com>
2024-08-20 02:13:09 +0900

    feat: 이미지 비율 및 콘텐츠 스케일 옵션 추가

--- app/src/main/java/nextstep/shoppingcart/ui/product/list/ProductCard.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product/list/ProductCard.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -22,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -59,7 +61,11 @@ internal fun ProductCard(
                         id = R.string.product_image_content_description,
                         item.product.name,
                     ),
-                modifier = Modifier.fillMaxWidth(),
+                contentScale = ContentScale.Crop,
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f),
             )
 
             if (item.isInCart) {
