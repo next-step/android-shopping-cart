f9d07d6d72688ba4e72155bb71c71be7364c1b36
ezhoon <dlwlgns1240@gmail.com>
2024-08-19 14:55:21 +0900

    step2 리뷰 반영

--- app/src/main/java/nextstep/shoppingcart/ui/screen/detail/ProductDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/detail/ProductDetailScreen.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -19,6 +21,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -41,7 +44,7 @@ fun ProductDetailRoute(
     onCartClick: (id: String) -> Unit,
     id: String,
 ) {
-    val product = dummyProductModels.find { it.id == id }
+    val product = remember { dummyProductModels.find { it.id == id } }
     if (product != null) {
         ProductDetailScreen(
             modifier = modifier,
@@ -64,9 +67,14 @@ private fun ProductDetailScreen(
     modifier: Modifier = Modifier
 ) {
     Scaffold(
+        modifier = modifier,
         topBar = { ProductDetailTopAppBar { onNavigationClick() } }
     ) {
-        Column(modifier.padding(it)) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .verticalScroll(rememberScrollState()),
+        ) {
             ProductImage(productName = itemName, imageUrl = itemImageUrl)
 
             Text(
