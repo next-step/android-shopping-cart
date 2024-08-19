2a417ed192aba6d2a3fed754b7524148dae0810a
jay.lee <stkment@gmail.com>
2024-08-19 14:00:24 +0900

    test: ProductListScreen test 추가

---
+++ app/src/androidTest/java/nextstep/shoppingcart/ui/product/list/ProductListScreenTest.kt
@@ -0,0 +1,26 @@
+package nextstep.shoppingcart.ui.product.list
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import org.junit.Rule
+import org.junit.Test
+
+class ProductListScreenTest {
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun 장바구니_담기_버튼으로_최초_상품_추가_후_상품_수량_조절기가_노출된다() {
+    }
+
+    @Test
+    fun 상품_조절기를_통해서_상품_수량_추가_시_상품_수량이_증가된다() {
+    }
+
+    @Test
+    fun 상품_조절기를_통해서_상품_수량_감소_시_상품_수량이_감소된다() {
+    }
+
+    @Test
+    fun 상품이_1개일_때_상품_수량_감소_시_조절기가_사라지고_장바구니_담기_버튼이_된다() {
+    }
+}
