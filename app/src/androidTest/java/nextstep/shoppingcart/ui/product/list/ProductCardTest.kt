8192bd2c7fb766e4ca284363bb6511804379c32f
jay.lee <stkment@gmail.com>
2024-08-20 11:33:51 +0900

    test: ProductCardTest 리팩토링

--- app/src/androidTest/java/nextstep/shoppingcart/ui/product/list/ProductCardTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/ui/product/list/ProductCardTest.kt
@@ -2,17 +2,17 @@ package nextstep.shoppingcart.ui.product.list
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.performClick
 import androidx.test.platform.app.InstrumentationRegistry
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.data.PRODUCT_LIST_MOCK_DATA
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -20,26 +20,26 @@ class ProductCardTest {
     @get:Rule
     val composeTestRule = createComposeRule()
     private val context = InstrumentationRegistry.getInstrumentation().targetContext
+    private var quantity by mutableIntStateOf(1)
 
-    @Test
-    fun 장바구니_담기_버튼으로_최초_상품_추가_후_상품_수량_조절기가_노출된다() {
+    @Before
+    fun setUp() {
         composeTestRule.setContent {
             val product = PRODUCT_LIST_MOCK_DATA.first()
-            var quantity by remember { mutableIntStateOf(0) }
             ProductCard(
                 product = product,
                 quantity = quantity,
                 onCardClick = {},
-                onAddToCartClick = { quantity++ },
+                onAddToCartClick = {},
                 onAddQuantityClick = {},
                 onRemoveQuantityClick = {},
             )
         }
+    }
 
-        composeTestRule
-            .onNodeWithTag(context.getString(R.string.test_tag_product_cart_add))
-            .assertIsDisplayed()
-            .performClick()
+    @Test
+    fun 장바구니_담기_버튼으로_최초_상품_추가_후_상품_수량_조절기가_노출된다() {
+        quantity = 1
 
         composeTestRule
             .onNodeWithTag(
@@ -50,26 +50,11 @@ class ProductCardTest {
 
     @Test
     fun 상품_조절기를_통해서_상품_수량_추가_시_상품_수량이_증가된다() {
-        composeTestRule.setContent {
-            val product = PRODUCT_LIST_MOCK_DATA.first()
-            var quantity by remember { mutableIntStateOf(1) }
-            ProductCard(
-                product = product,
-                quantity = quantity,
-                onCardClick = {},
-                onAddToCartClick = {},
-                onAddQuantityClick = {
-                    quantity++
-                },
-                onRemoveQuantityClick = {},
-            )
-        }
+        // given
+        quantity = 1
 
-        composeTestRule
-            .onNodeWithTag(
-                context.getString(R.string.test_tag_quantity_selector_add),
-            ).assertIsDisplayed()
-            .performClick()
+        // when
+        quantity = 2
 
         composeTestRule
             .onNodeWithTag(
@@ -80,23 +65,11 @@ class ProductCardTest {
 
     @Test
     fun 상품_조절기를_통해서_상품_수량_감소_시_상품_수량이_감소된다() {
-        composeTestRule.setContent {
-            val product = PRODUCT_LIST_MOCK_DATA.first()
-            var quantity by remember { mutableIntStateOf(2) }
-            ProductCard(
-                product = product,
-                quantity = quantity,
-                onCardClick = {},
-                onAddToCartClick = {},
-                onAddQuantityClick = {},
-                onRemoveQuantityClick = { quantity-- },
-            )
-        }
+        // gvien
+        quantity = 2
 
-        composeTestRule
-            .onNodeWithTag(context.getString(R.string.test_tag_quantity_selector_remove))
-            .assertIsDisplayed()
-            .performClick()
+        // when
+        quantity = 1
 
         composeTestRule
             .onNodeWithTag(
@@ -107,30 +80,18 @@ class ProductCardTest {
 
     @Test
     fun 상품이_수량이_없어지면_수량_조절기가_사라지고_장바구니_담기_버튼이_노출된다() {
-        composeTestRule.setContent {
-            val product = PRODUCT_LIST_MOCK_DATA.first()
-            var quantity by remember { mutableIntStateOf(1) }
-            ProductCard(
-                product = product,
-                quantity = quantity,
-                onCardClick = {},
-                onAddToCartClick = {},
-                onAddQuantityClick = {},
-                onRemoveQuantityClick = { quantity-- },
-            )
-        }
+        // given
+        quantity = 1
 
-        composeTestRule
-            .onNodeWithTag(context.getString(R.string.test_tag_quantity_selector_remove))
-            .assertIsDisplayed()
-            .performClick()
+        // when
+        quantity = 0
 
         composeTestRule
             .onNodeWithTag(context.getString(R.string.test_tag_quantity_selector))
             .assertIsNotDisplayed()
 
         composeTestRule
-            .onNodeWithTag(context.getString(R.string.test_tag_product_cart_add))
+            .onNodeWithContentDescription(context.getString(R.string.add))
             .assertIsDisplayed()
     }
 }
