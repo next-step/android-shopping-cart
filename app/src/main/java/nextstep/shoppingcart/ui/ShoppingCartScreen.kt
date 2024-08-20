1c4076ca259e6ba5f66cf35b313f2ea42d709817
diana <thxall92@github.com>
2024-08-21 01:06:28 +0900

    feat: 상품 목록 화면 구현

---
+++ app/src/main/java/nextstep/shoppingcart/ui/ShoppingCartScreen.kt
@@ -0,0 +1,82 @@
+package nextstep.shoppingcart.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.data.dummyProducts
+import nextstep.shoppingcart.ui.component.ProductItem
+import nextstep.shoppingcart.ui.theme.BlackTitle
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingCartScreen() {
+
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+
+        topBar = {
+            CenterAlignedTopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color.White,
+                    titleContentColor = BlackTitle,
+                ),
+                title = {
+                    Text(
+                        text = "상품 목록",
+                        fontSize = 22.sp
+                    )
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.ShoppingCart,
+                            contentDescription = "Cart"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        },
+    ) { innerPadding ->
+        LazyVerticalGrid(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+            columns = GridCells.Fixed(2),
+            modifier = Modifier
+                .background(Color.White)
+                .padding(vertical = 13.dp, horizontal = 18.dp)
+                .padding(innerPadding)
+        ) {
+            items(dummyProducts.size) { item ->
+                ProductItem(dummyProducts[item])
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShoppingCartScreenPreview() {
+    ShoppingCartScreen()
+}
