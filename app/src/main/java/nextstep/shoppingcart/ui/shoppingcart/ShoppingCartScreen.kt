6c2afaeae846cc3ee55beb292a2ef384c7e6bcf6
s9hn <tnghqn@naver.com>
2024-08-20 21:38:52 +0900

    feat: ShoppingCartScreen 구현 및 적용

--- app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
@@ -1,25 +1,35 @@
 package nextstep.shoppingcart.ui.shoppingcart
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import nextstep.shoppingcart.R
+import nextstep.shoppingcart.ui.component.ShoppingButton
 import nextstep.shoppingcart.ui.component.ShoppingTopBar
+import nextstep.shoppingcart.ui.shoppingcart.component.ShoppingCartLazyColumn
 
 @Composable
 fun ShoppingCartScreen(
     onBackClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        modifier = modifier.fillMaxSize()
-    ) {
+    Column(modifier = modifier.fillMaxSize()) {
         ShoppingTopBar(
             title = stringResource(id = R.string.shopping_cart_title),
             onBackClick = onBackClick,
+        )
+        Spacer(modifier = Modifier.height(height = 16.dp))
+        ShoppingCartLazyColumn()
+        Spacer(modifier = Modifier.weight(weight = 1f))
+        ShoppingButton(
+            onClick = {},
+            buttonTitle = "",
         )
     }
 }
