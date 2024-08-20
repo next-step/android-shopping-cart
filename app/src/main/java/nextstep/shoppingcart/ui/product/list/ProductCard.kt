da5a28cf41ec7b23fbe55d637538dcc24fc80de6
jay.lee <stkment@gmail.com>
2024-08-20 11:02:30 +0900

    feat: FloatingActionButton으로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/product/list/ProductCard.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product/list/ProductCard.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
@@ -24,7 +25,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -91,19 +91,22 @@ internal fun ProductCard(
                             ).background(Color.White),
                 )
             } else {
-                AddToCartButton(
+                FloatingActionButton(
                     onClick = onAddToCartClick,
+                    shape = CircleShape,
+                    containerColor = Color.White,
                     modifier =
                         Modifier
                             .padding(12.dp)
                             .align(Alignment.BottomEnd)
-                            .border(
-                                width = 1.dp,
-                                color = Color.Gray,
-                                shape = CircleShape,
-                            ).size(42.dp)
-                            .testTag(stringResource(id = R.string.test_tag_product_cart_add)),
-                )
+                            .size(42.dp),
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = stringResource(id = R.string.add),
+                        modifier = Modifier.size(24.dp),
+                    )
+                }
             }
         }
         ProductCardDescription(
