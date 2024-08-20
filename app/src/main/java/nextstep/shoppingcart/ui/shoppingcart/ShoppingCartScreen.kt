051b1e7ddb2a52c6f378ab69323fe6bf0cd587aa
s9hn <tnghqn@naver.com>
2024-08-21 00:43:05 +0900

    refactor: Cart 업데이트 및 UI에 전달

--- app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
@@ -5,6 +5,11 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -26,21 +31,36 @@ fun ShoppingCartScreen(
     modifier: Modifier = Modifier,
 ) {
     Column(modifier = modifier.fillMaxSize()) {
+        var cartProducts by remember { mutableStateOf(items) }
+        var total by remember { mutableLongStateOf(totalPrice) }
+
         ShoppingTopBar(
             title = stringResource(id = shopping_cart_title),
             onBackClick = onBackClick,
         )
         Spacer(modifier = Modifier.height(height = 16.dp))
         ShoppingCartLazyColumn(
-            cartProducts = items,
-            onRemoveClick = { cartProductId -> removeAll(findProductById(cartProductId)) },
-            onSubtractClick = { cartProductId -> addOne(findProductById(cartProductId)) },
-            onAddClick = { cartProductId -> addOne(findProductById(cartProductId)) },
+            cartProducts = cartProducts,
+            onRemoveClick = { cartProductId ->
+                removeAll(findProductById(cartProductId))
+                cartProducts = items
+                total = totalPrice
+            },
+            onSubtractClick = { cartProductId ->
+                addOne(findProductById(cartProductId))
+                cartProducts = items
+                total = totalPrice
+            },
+            onAddClick = { cartProductId ->
+                addOne(findProductById(cartProductId))
+                cartProducts = items
+                total = totalPrice
+            },
         )
         Spacer(modifier = Modifier.weight(weight = 1f))
         ShoppingButton(
-            onClick = {},
-            buttonTitle = stringResource(id = shopping_cart_total_format, totalPrice),
+            onClick = { },
+            buttonTitle = stringResource(id = shopping_cart_total_format, total),
         )
     }
 }
