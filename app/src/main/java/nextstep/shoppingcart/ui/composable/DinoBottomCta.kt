2057e9ee167bcf26700c343351d6f6b00f787ccc
dino <sjjeong1225@gmail.com>
2024-08-21 17:15:38 +0900

    refactor: DinoBottomCta 구현을 TextButton으로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/composable/DinoBottomCta.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/composable/DinoBottomCta.kt
@@ -1,17 +1,19 @@
 package nextstep.shoppingcart.ui.composable
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -21,7 +23,7 @@ fun BoxScope.DinoBottomCta(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Button(
+    TextButton(
         modifier = Modifier
             .fillMaxWidth()
             .align(Alignment.BottomCenter),
@@ -40,4 +42,17 @@ fun BoxScope.DinoBottomCta(
         )
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DinoBottomCtaPreview() {
+    Box {
+        DinoBottomCta(
+            ctaText = "버튼",
+            onClick = {
+
+            }
+        )
+    }
 }
