dd7c9bf75d83f523674a3770bfd73e8f05c3bfa0
s9hn <tnghqn@naver.com>
2024-08-21 14:48:15 +0900

    refactor: ShoppingProductImage width, height 제거

--- app/src/main/java/nextstep/shoppingcart/ui/component/ShoppingProductImage.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/component/ShoppingProductImage.kt
@@ -1,12 +1,9 @@
 package nextstep.shoppingcart.ui.component
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import nextstep.shoppingcart.R.string.shopping_item_description
 import nextstep.shoppingcart.data.dummyProducts
@@ -20,9 +17,7 @@ fun ShoppingProductImage(
     AsyncImage(
         model = product.imageUrl,
         contentDescription = stringResource(id = shopping_item_description),
-        modifier = modifier
-            .width(width = 360.dp)
-            .height(height = 360.dp),
+        modifier = modifier,
     )
 }
 
