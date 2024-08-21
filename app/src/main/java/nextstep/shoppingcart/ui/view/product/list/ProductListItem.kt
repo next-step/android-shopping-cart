8293ec22ee1ed3f32011b1dd2f2c7e630295dbcc
dino <sjjeong1225@gmail.com>
2024-08-21 20:43:57 +0900

    feat: 상품목록에서 장바구니 담기 기능 추가

--- app/src/main/java/nextstep/shoppingcart/ui/view/product/list/ProductListItem.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/view/product/list/ProductListItem.kt
@@ -1,16 +1,28 @@
 package nextstep.shoppingcart.ui.view.product.list
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -19,32 +31,78 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.SubcomposeAsyncImage
 import nextstep.shoppingcart.R
+import nextstep.shoppingcart.ui.composable.Stepper
+import nextstep.shoppingcart.ui.model.CartItem
 import nextstep.shoppingcart.ui.model.Product
-import nextstep.shoppingcart.ui.model.dummyProducts
 
 @Composable
 fun ProductListItem(
     product: Product,
+    countInCart: Int,
     onClick: (Product) -> Unit,
+    onQuantityChange: (CartItem, Int) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     Column(
         modifier = modifier
             .clickable { onClick(product) },
     ) {
-        SubcomposeAsyncImage(
-            modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1f),
-            model = product.imageUrl,
-            contentDescription = product.name,
-            loading = {
-                CircularProgressIndicator()
-            },
-            success = {
-                Image(painter = it.painter, contentDescription = "")
+        Box {
+            SubcomposeAsyncImage(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(1f),
+                model = product.imageUrl,
+                contentDescription = product.name,
+                loading = {
+                    CircularProgressIndicator()
+                },
+                success = {
+                    Image(painter = it.painter, contentDescription = "")
+                }
+            )
+
+            if (countInCart == 0) {
+                IconButton(
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .size(42.dp)
+                        .background(
+                            color = Color.White,
+                            shape = CircleShape
+                        )
+                        .padding(9.dp),
+                    onClick = {
+                        onQuantityChange(
+                            CartItem(product, countInCart),
+                            countInCart + 1
+                        )
+                    },
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Add,
+                        contentDescription = "",
+                    )
+                }
+            } else {
+                Stepper(
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                        .background(
+                            color = Color.White,
+                            shape = RoundedCornerShape(size = 4.dp)
+                        )
+                        .padding(bottom = 12.dp),
+                    count = countInCart,
+                    onCountChange = {
+                        onQuantityChange(
+                            CartItem(product, countInCart),
+                            it,
+                        )
+                    }
+                )
             }
-        )
+        }
         Spacer(modifier = Modifier.height(8.dp))
         Text(
             modifier = Modifier.fillMaxWidth(),
@@ -63,7 +121,7 @@ fun ProductListItem(
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun ProductListItemPreview() {
     ProductListItem(
@@ -72,6 +130,8 @@ private fun ProductListItemPreview() {
             price = 1000,
             imageUrl = "https://example.com/image.jpg",
         ),
+        countInCart = 0,
         onClick = {},
+        onQuantityChange = { _, _ -> },
     )
 }
