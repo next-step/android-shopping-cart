07aff89a3affb25c0454769d455798d89b6bef25
Jihoi-Kang <jihoi.kang@gmail.com>
2024-08-22 14:39:52 +0900

    refactor: add cart count on product list and update observable cart

--- app/src/main/java/nextstep/shoppingcart/data/Cart.kt
+++ app/src/main/java/nextstep/shoppingcart/data/Cart.kt
@@ -1,15 +1,18 @@
 package nextstep.shoppingcart.data
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import nextstep.shoppingcart.model.CartItem
 import nextstep.shoppingcart.model.Product
 
 object Cart {
-    private val _items: MutableList<CartItem> = mutableListOf()
-    val items: List<CartItem> get() = _items.toList()
+    private val _items: SnapshotStateList<CartItem> = mutableStateListOf()
+    val items: SnapshotStateList<CartItem> get() = _items
 
     val totalPrice: Long get() = _items.sumOf { it.totalPrice }
 
-    fun addOne(product: Product): List<CartItem> {
+    fun addOne(product: Product) {
         val item = _items.find { it.product == product }
         if (item == null) {
             _items.add(CartItem(product, 1))
@@ -17,10 +20,9 @@ object Cart {
             val index = _items.indexOf(item)
             _items[index] = item.copy(count = item.count + 1)
         }
-        return items
     }
 
-    fun removeOne(product: Product): List<CartItem> {
+    fun removeOne(product: Product) {
         _items.find { it.product == product }
             ?.let { item ->
                 if (item.count > 1) {
@@ -30,7 +32,6 @@ object Cart {
                     _items.remove(item)
                 }
             }
-        return items
     }
 
     fun removeAll(product: Product): List<CartItem> {
