92ba0c215587a9fc559f36d9e9a06e70798f1b57
OYJ <oyj7677@gmail.com>
2024-08-22 11:28:35 +0900

    Test : 상품 카드 수량 조절 기능 테스트 코드 작성

--- app/src/androidTest/java/nextstep/shoppingcart/InteractiveProductImageTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/InteractiveProductImageTest.kt
@@ -2,6 +2,7 @@ package nextstep.shoppingcart
 
 import android.content.Context
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.performClick
@@ -69,6 +70,58 @@ class InteractiveProductImageTest {
         // then : 상품 카드에 수량 컴포넌트가 나타난다.
         composeTestRule
             .onNodeWithTag(context.getString(R.string.test_tag_interactive_quantity) + product.name)
+            .assertExists()
+    }
+
+    @Test
+    fun 상품_카드의_수량_조절_컴포넌트의_더하기_버튼을_클릭하면_수량_text가_1_증가하여_노출된다() {
+        // given : 상품 목록을 추가하고, 카트에 상품을 추가한다.
+        val product = Product(
+            id = 1,
+            imgUrl = "https://picsum.photos/seed/1/200",
+            name = "상품 1-이름이 너무 길다면 어떻게 할 것인가요?",
+            price = 3000
+        )
+        Cart.addOne(product)
+
+        composeTestRule.setContent {
+            InteractiveProductImage(product, Modifier)
+        }
+
+        // when : 상품 카드의 수량 컴포넌트를 클릭한다(더하기)
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_plus_icon, product.name))
+            .performClick()
+
+        // then : 상품 수량이 증가한다.
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_quantity_text, product.name))
+            .assertTextEquals("2")
+    }
+
+    @Test
+    fun 상품_수량이_0일경우_다시_fab이_노출된다() {
+        // given : 상품 목록을 추가하고, 카트에 상품을 추가한다.
+        val product = Product(
+            id = 1,
+            imgUrl = "https://picsum.photos/seed/1/200",
+            name = "상품 1-이름이 너무 길다면 어떻게 할 것인가요?",
+            price = 3000
+        )
+        Cart.addOne(product)
+        composeTestRule.setContent {
+            InteractiveProductImage(product, Modifier)
+        }
+
+        // when : - 버튼을 클릭하여 수량을 0으로 만든다.
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_minus_icon, product.name))
+            .performClick()
+
+
+        // then : fab컴포넌트가 나타난다.
+        composeTestRule
+            .onNodeWithTag(context.getString(R.string.test_tag_add_fab) + product.name)
             .assertExists()
     }
 }
