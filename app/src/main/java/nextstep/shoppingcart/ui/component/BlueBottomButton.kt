9f4034a7abe9cd3eb3895585da12a13bce7775f8
diana <thxall92@github.com>
2024-09-01 22:48:28 +0900

    refactor: 주문하기 버튼의 텍스트 프리뷰 추가

--- app/src/main/java/nextstep/shoppingcart/ui/component/BlueBottomButton.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/component/BlueBottomButton.kt
@@ -11,10 +11,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.R
 import nextstep.shoppingcart.ui.theme.Blue50
 
 @Composable
@@ -48,6 +50,16 @@ fun ProductDetailCartAddButtonPreview() {
     Box {
         BlueBottomButton(
             "장바구니 담기", {}
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun OrderButtonPreview() {
+    Box {
+        BlueBottomButton(
+            stringResource(id = R.string.price_format_button_label, 1000), {}
         )
     }
 }
