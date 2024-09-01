bbbc9b76a3439f5548e755dd5ca0bda6c23d6c52
diana <thxall92@github.com>
2024-09-01 22:46:33 +0900

    refactor: CartList가 비어있을 떼 CartContent를 그리지 않도록 수정

--- app/src/androidTest/java/nextstep/shoppingcart/ui/cart/ShoppingCartScreenTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/ui/cart/ShoppingCartScreenTest.kt
@@ -59,7 +59,7 @@ internal class CartScreenTest {
 
         // then : "테스트 상품" 이름의 상품이 없어짐
         composeTestRule
-            .onNodeWithTag("테스트 상품")
+            .onNodeWithTag("cartItem")
             .assertDoesNotExist()
     }
 
