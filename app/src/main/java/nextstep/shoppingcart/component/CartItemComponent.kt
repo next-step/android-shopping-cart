fac86ed185a23872f5b26efe0d7386664fd5cede
eshc123 <>
2024-09-01 17:24:18 +0900

    refactor: 불필요한 wrapContent~ 삭제

--- app/src/main/java/nextstep/shoppingcart/component/CartItemComponent.kt
+++ app/src/main/java/nextstep/shoppingcart/component/CartItemComponent.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
@@ -105,7 +104,6 @@ fun CartItemHeader(
     ) {
         Text(
             modifier = Modifier
-                .wrapContentHeight()
                 .weight(1f),
             text = product.name,
             style = MaterialTheme.typography.titleSmall,
