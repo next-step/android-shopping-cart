bc19bcc8459e28fc28d990aa9d1546bcd48eb432
Bandal <no1msh1217@gmail.com>
2025-02-13 21:36:42 +0900

    feat: 상품 목록을 표시

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -3,15 +3,22 @@ package nextstep.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import nextstep.shoppingcart.data.FakeProductRepository
+import nextstep.shoppingcart.domain.Products
 import nextstep.shoppingcart.screens.MainScreen
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class MainActivity : ComponentActivity() {
+    private val products: Products = FakeProductRepository.getAllProducts()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            enableEdgeToEdge()
             ShoppingCartTheme {
                 MainScreen(
+                    products = products,
                     onActionClick = { /* TODO: 장바구니 이동 로직 구현(2단계 미션) */ }
                 )
             }
--- app/src/main/java/nextstep/shoppingcart/screens/MainScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/screens/MainScreen.kt
@@ -6,26 +6,35 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.components.ShoppingCartTopBar
 import nextstep.shoppingcart.components.ShoppingCartTopBarActionType
+import nextstep.shoppingcart.data.FakeProductRepository
+import nextstep.shoppingcart.domain.Products
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 @Composable
-internal fun MainScreen(onActionClick: () -> Unit) {
+internal fun MainScreen(
+    products: Products,
+    onActionClick: () -> Unit,
+) {
     Scaffold(
         topBar = {
             ShoppingCartTopBar(
-                title = R.string.item_list_top_bar_title,
+                title = R.string.product_list_top_bar_title,
                 action = ShoppingCartTopBarActionType.CART,
                 onActionClick = onActionClick,
             )
         },
         modifier = Modifier.fillMaxSize(),
-        containerColor = MaterialTheme.colorScheme.background
+        containerColor = Color.White
     ) { paddingValues ->
-        ItemListScreen(modifier = Modifier.padding(paddingValues))
+        ProductListScreen(
+            products = products,
+            modifier = Modifier.padding(paddingValues)
+        )
     }
 }
 
@@ -34,6 +43,7 @@ internal fun MainScreen(onActionClick: () -> Unit) {
 private fun MainScreenPreview() {
     ShoppingCartTheme {
         MainScreen(
+            products = FakeProductRepository.getAllProducts(),
             onActionClick = {},
         )
     }
