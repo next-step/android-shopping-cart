1d7dee20dafc99d80ea7b7e5f9753af069a5b0bc
Bandal <no1msh1217@gmail.com>
2025-02-13 21:35:25 +0900

    refactor: ProductListScreen으로 이름 변경

--- app/src/main/java/nextstep/shoppingcart/components/ShoppingCartTopBar.kt
+++ app/src/main/java/nextstep/shoppingcart/components/ShoppingCartTopBar.kt
@@ -8,8 +8,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,6 +38,7 @@ internal fun ShoppingCartTopBar(
                 )
             }
         },
+        colors = TopAppBarDefaults.topAppBarColors().copy(containerColor = Color.White),
         modifier = modifier,
     )
 }
@@ -46,7 +49,7 @@ internal enum class ShoppingCartTopBarActionType(
 ) {
     CART(
         icon = Icons.Filled.ShoppingCart,
-        contentDescription = R.string.item_list_top_bar_action_cart,
+        contentDescription = R.string.product_list_top_bar_action_cart,
     ),
 }
 
@@ -55,7 +58,7 @@ internal enum class ShoppingCartTopBarActionType(
 private fun ShoppingCartTopBarPreview() {
     ShoppingCartTheme {
         ShoppingCartTopBar(
-            title = R.string.item_list_top_bar_title,
+            title = R.string.product_list_top_bar_title,
             action = ShoppingCartTopBarActionType.CART,
             onActionClick = {},
         )
