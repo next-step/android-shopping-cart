ed5d3cf35d9b4cdd819631c7e15f30b740eae0c3
DoTheBestMayB <send1684@gmail.com>
2025-02-14 02:34:32 +0900

    feat: enableEdgeToEdge 적용하기

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -3,12 +3,15 @@ package nextstep.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import nextstep.shoppingcart.ui.product_list.ProductListScreen
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+
         setContent {
             ShoppingCartTheme {
                 ProductListScreen()
