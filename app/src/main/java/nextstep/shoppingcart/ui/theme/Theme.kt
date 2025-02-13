0436325619f5db5193a1758c001d664fce34172a
DoTheBestMayB <send1684@gmail.com>
2025-02-13 21:56:16 +0900

    style: Color Theme 정의하기

--- app/src/main/java/nextstep/shoppingcart/ui/theme/Theme.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/Theme.kt
@@ -22,7 +22,7 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
+    primary = Blue50,
     secondary = PurpleGrey40,
     tertiary = Pink40
 
