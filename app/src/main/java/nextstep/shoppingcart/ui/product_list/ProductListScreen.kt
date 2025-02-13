ebebc95e94106ba50a48aca5edd43191d827869c
DoTheBestMayB <send1684@gmail.com>
2025-02-14 02:10:59 +0900

    style: top app bar 배경색을 흰색으로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/product_list/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/product_list/ProductListScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -91,7 +92,10 @@ fun ProductListTopBar(
                     contentDescription = stringResource(R.string.product_list_description_shopping_cart),
                 )
             }
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors().copy(
+            containerColor = Color.White,
+        )
     )
 }
 
