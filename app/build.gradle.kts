367f3df967644ee435b0555e5f49e59062329fbf
yangsooplus <yangsooplus@naver.com>
2025-02-14 22:32:31 +0900

    chore: coil 의존성, 인터넷 권한 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
