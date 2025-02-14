a83f3cd51703c79d59ff8a9d6495fc73517480b2
seungohjin <corrobolea@gmail.com>
2025-02-15 08:52:25 +0900

    feat: 앱바 타이틀 구현

---
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductScreen.kt
@@ -0,0 +1,42 @@
+package nextstep.shoppingcart.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProductScreen() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = stringResource(id = R.string.appbar_product_title))
+                },
+            )
+        },
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+                .padding(it)
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ProductPreview() {
+    ProductScreen()
+}
