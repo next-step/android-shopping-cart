93ff16eddbcb99839405b6b31ea98a445dfc6b7d
seungohjin <corrobolea@gmail.com>
2025-02-15 08:52:37 +0900

    feat: 앱바 장바구니 액션 버튼 구현

--- app/src/main/java/nextstep/shoppingcart/ui/screen/ProductScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductScreen.kt
@@ -2,13 +2,18 @@ package nextstep.shoppingcart.ui.screen
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.R
@@ -23,6 +28,17 @@ fun ProductScreen() {
                 title = {
                     Text(text = stringResource(id = R.string.appbar_product_title))
                 },
+                actions = {
+                    IconButton(
+                        onClick = { }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ShoppingCart,
+                            contentDescription = "Cart",
+                            tint = Color.Black
+                        )
+                    }
+                }
             )
         },
     ) {
