32a829bb1548d4043e70474ab881fd78c9ea8496
DoTheBestMayB <send1684@gmail.com>
2025-02-16 00:52:43 +0900

    build: Navigation Component 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.core.splashscreen)
+    implementation(libs.androidx.navigation.compose)
 
     implementation(libs.coil.compose)
     implementation(libs.coil.network.okhttp)
