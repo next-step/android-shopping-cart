d392ca5dab4402749e085b6a43685ddccf22d841
sanga.oh2 <66289794+sana-20@users.noreply.github.com>
2025-02-15 17:58:50 +0900

    feat: 상품 목록 TopBar UI 구현

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 ### step1 기능 목록
 - [x] 상품 목록 Screen 구현
-- [ ] 상품 목록 TopBar UI 구현
+- [x] 상품 목록 TopBar UI 구현
 - [ ] 상품 목록 Item UI 구현 
 - [ ] 상품 목록 리스트 UI 구현
--- app/src/main/java/nextstep/shoppingcart/screen/ProductsScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/screen/ProductsScreen.kt
@@ -1,9 +1,24 @@
 package nextstep.shoppingcart.screen
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.R
 
 @Composable
 fun ProductsScreen() {
@@ -18,9 +33,33 @@ fun ProductsScreen() {
     }
 }
 
+@Preview
 @Composable
-private fun ProductsTopBar() {
-
+private fun ProductsTopBar(
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .padding(horizontal = 16.dp),
+    ) {
+        Text(
+            text = stringResource(id = R.string.products_top_bar_title),
+            fontSize = 22.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .align(Alignment.Center)
+        )
+        Icon(
+            imageVector = Icons.Filled.ShoppingCart,
+            contentDescription = "shooping cart",
+            modifier = Modifier
+                .align(Alignment.CenterEnd)
+                .clickable { }
+        )
+    }
 }
 
 @Composable
