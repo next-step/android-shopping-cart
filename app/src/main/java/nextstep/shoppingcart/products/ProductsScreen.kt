8c55408553b8b3d6b284c570f02f73f0397d41c8
ver <ver@sendy.ai>
2025-02-15 20:55:42 +0900

    feat: 상품 목록 탑바를 구현한다

--- app/src/main/java/nextstep/shoppingcart/products/ProductsScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/products/ProductsScreen.kt
@@ -2,8 +2,10 @@ package nextstep.shoppingcart.products
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -17,8 +19,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
 import nextstep.shoppingcart.R
+import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.theme.grey10
+import nextstep.shoppingcart.ui.theme.grey40
 
 @Composable
 private fun ProductsTopAppBar(modifier: Modifier = Modifier) {
@@ -51,6 +57,33 @@ private fun ProductsTopAppBar(modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+private fun ProductItem(
+    product: Product,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        AsyncImage(
+            model = product.imageUrl,
+            contentDescription = "상품 이미지",
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f)
+                .clickable(onClick = onClick),
+            placeholder = painterResource(R.drawable.ic_launcher_background),
+            error = painterResource(R.drawable.ic_launcher_background)
+        )
+        Text(text = product.name, color = grey40, fontSize = 16.sp, fontWeight = FontWeight.W700)
+        Text(
+            text = stringResource(R.string.all_price_format).format(product.price),
+            color = grey40,
+            fontSize = 16.sp,
+            fontWeight = FontWeight.W400
+        )
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 private fun ProductsTopBarPreview() {
@@ -58,3 +91,20 @@ private fun ProductsTopBarPreview() {
         ProductsTopAppBar()
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductItemPreview() {
+    MaterialTheme {
+        ProductItem(
+            Product(
+                id = 1L,
+                name = "상품1",
+                price = 10000L,
+                imageUrl = "https://picsum.photos/200",
+            ),
+            onClick = {}
+        )
+    }
+}
+
