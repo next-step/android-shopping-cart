bec7b1d504e6661ffcab3db93378fe13dcfa7b22
seungohjin <corrobolea@gmail.com>
2025-02-15 10:34:16 +0900

    feat: Coil 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
 }
