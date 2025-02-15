2809aa5f6885f4694f0b6a7af60684b6f1c1e120
Bandal <no1msh1217@gmail.com>
2025-02-15 13:56:42 +0900

    feat: Product Null object 구현

--- app/src/main/java/nextstep/shoppingcart/domain/model/Product.kt
+++ app/src/main/java/nextstep/shoppingcart/domain/model/Product.kt
@@ -5,4 +5,13 @@ data class Product(
     val name: String,
     val price: Price,
     val imageUrl: String,
-)
+) {
+    companion object {
+        val NotFound = Product(
+            id = Int.MIN_VALUE,
+            name = "No product found",
+            price = Price.of(0),
+            imageUrl = ""
+        )
+    }
+}
