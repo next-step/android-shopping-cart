ba80bf11f0c3d74fafbf486e49622925c50a2830
DoTheBestMayB <send1684@gmail.com>
2025-02-16 01:15:54 +0900

    refactor: splashScreen 삭제하기

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.androidx.core.splashscreen)
     implementation(libs.androidx.navigation.compose)
 
     implementation(libs.coil.compose)
--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import nextstep.shoppingcart.navigation.NavigationRoot
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
