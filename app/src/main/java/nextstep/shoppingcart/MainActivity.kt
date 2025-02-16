eed64e229bf7236756a8265b9d7873905e4bb576
parkduksung <duksung1234@naver.com>
2025-02-16 21:11:26 +0900

    refactor: MainActivity 변경

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -1,18 +1,29 @@
 package nextstep.shoppingcart
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import nextstep.shoppingcart.designsystem.theme.ShoppingCartTheme
+import androidx.compose.runtime.Composable
+import nextstep.shoppingcart.base.BaseComponentActivity
+import nextstep.shoppingcart.data.mockProducts
+import nextstep.shoppingcart.navigation.RouteType
+import nextstep.shoppingcart.ui.cart.CartActivity
+import nextstep.shoppingcart.ui.detail.ProductDetailActivity
 import nextstep.shoppingcart.ui.list.ProductListScreen
 
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ShoppingCartTheme {
-                ProductListScreen()
-            }
+class MainActivity : BaseComponentActivity() {
+
+    override val rootComponent: @Composable () -> Unit
+        get() = {
+            ProductListScreen(
+                productList = mockProducts,
+                onRoute = { type ->
+                    when (type) {
+                        RouteType.ToCart -> {
+                            startActivity(CartActivity.newInstance(context = this))
+                        }
+                        is RouteType.ToDetail -> {
+                            startActivity(ProductDetailActivity.newInstance(this, type.item))
+                        }
+                    }
+                },
+            )
         }
-    }
 }
