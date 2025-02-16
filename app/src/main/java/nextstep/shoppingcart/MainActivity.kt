16272b283dc88350ed7156430671f1f47df0d10b
ver <ver@sendy.ai>
2025-02-16 13:18:03 +0900

    feat: 상품 목록에서 상품을 누르면 상품 상세 화면으로 이동한다.

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import nextstep.shoppingcart.model.Product
+import nextstep.shoppingcart.productdetail.ProductDetailActivity
 import nextstep.shoppingcart.products.ProductsScreen
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
@@ -15,10 +17,14 @@ class MainActivity : ComponentActivity() {
                 enableEdgeToEdge()
                 ProductsScreen(
                     products = FakeProductsRepository.getProducts(),
-                    onProductClick = { /*TODO*/ }
+                    onProductClick = { product -> handleProductClick(product) }
                 )
             }
         }
+    }
+
+    private fun handleProductClick(product: Product) {
+        startActivity(ProductDetailActivity.getIntent(this, product.id))
     }
 }
 
---
+++ app/src/main/java/nextstep/shoppingcart/productdetail/ProductDetailActivity.kt
@@ -0,0 +1,37 @@
+package nextstep.shoppingcart.productdetail
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import nextstep.shoppingcart.FakeProductsRepository
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+
+class ProductDetailActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val productId = intent.getLongExtra(PRODUCT_ID, 0)
+        setContent {
+            ShoppingCartTheme {
+                enableEdgeToEdge()
+                ProductDetailScreen(
+                    product = FakeProductsRepository.getProduct(productId),
+                    onAddProductClick = { /*TODO*/ },
+                    onBackButtonClick = { /*TODO*/ }
+                )
+            }
+        }
+    }
+
+    companion object {
+        private const val PRODUCT_ID = "product_id"
+
+        fun getIntent(context: Context, productId: Long): Intent {
+            return Intent(context, ProductDetailActivity::class.java).apply {
+                putExtra(PRODUCT_ID, productId)
+            }
+        }
+    }
+}
