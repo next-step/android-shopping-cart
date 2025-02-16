611a4242e8fd95c9bae8e5d10fb2bfb6bfb5a030
ver <ver@sendy.ai>
2025-02-16 13:08:22 +0900

    feat: 상품 상세 화면을 구현한다.

--- app/src/main/java/nextstep/shoppingcart/productdetail/ProductDetailScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/productdetail/ProductDetailScreen.kt
@@ -1,10 +1,17 @@
 package nextstep.shoppingcart.productdetail
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
@@ -13,7 +20,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight.Companion.W700
@@ -25,21 +35,46 @@ import coil3.compose.AsyncImage
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+import nextstep.shoppingcart.ui.theme.blue50
 
 @Composable
 fun ProductDetailScreen(
     product: Product,
+    onAddProductClick: () -> Unit,
     onBackButtonClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Scaffold(
-        modifier = modifier,
+        modifier = modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .statusBarsPadding(),
         topBar = { ProductDetailTopAppBar(onBackButtonClick = onBackButtonClick) }
     ) { innerPadding ->
-        ProductDetailContent(
-            product = product,
-            modifier = Modifier.padding(paddingValues = innerPadding)
-        )
+        Box(modifier = Modifier.fillMaxSize()) {
+            ProductDetailContent(
+                product = product,
+                modifier = Modifier.padding(paddingValues = innerPadding)
+            )
+            Button(
+                onClick = onAddProductClick,
+                colors = ButtonDefaults.buttonColors().copy(
+                    containerColor = blue50,
+                    contentColor = Color.White
+                ),
+                shape = RectangleShape,
+                contentPadding = PaddingValues(16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.BottomCenter)
+            ) {
+                Text(
+                    text = stringResource(R.string.product_detail_btn_add_product_in_cart),
+                    fontSize = 20.sp,
+                    fontWeight = W700,
+                )
+            }
+        }
     }
 }
 
@@ -110,13 +145,14 @@ fun ProductDetailTopAppBar(
 private fun ProductDetailScreenPreview() {
     ShoppingCartTheme {
         ProductDetailScreen(
-            onBackButtonClick = {},
             product = Product(
                 id = 1,
                 name = "PET보틀-원형(500ml)",
                 price = 10000L,
                 imageUrl = "https://picsum.photos/200",
-            )
+            ),
+            onAddProductClick = {},
+            onBackButtonClick = {}
         )
     }
 }
