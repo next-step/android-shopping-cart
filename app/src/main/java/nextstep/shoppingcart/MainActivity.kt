79c8f3fce76ee507bab884c4e729206476cc6bd3
KimMooHyeon <dlfb77@gmail.com>
2025-02-17 02:00:42 +0900

    feat : 상품 목록 step1 화면구성

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -3,44 +3,19 @@ package nextstep.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+import nextstep.shoppingcart.ui.theme.shopping.ProductListScreen
+import nextstep.shoppingcart.ui.theme.shopping.lists
+import nextstep.shoppingcart.ui.theme.shopping.model.ProductListUiState
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val uiState = ProductListUiState.Products(lists)
         setContent {
             ShoppingCartTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ProductListScreen(uiState)
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ShoppingCartTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/ProductListScreen.kt
@@ -0,0 +1,163 @@
+package nextstep.shoppingcart.ui.theme.shopping
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+import nextstep.shoppingcart.ui.theme.shopping.component.ProductListContents
+import nextstep.shoppingcart.ui.theme.shopping.component.ProductListTopBar
+import nextstep.shoppingcart.ui.theme.shopping.model.Product
+import nextstep.shoppingcart.ui.theme.shopping.model.ProductListUiState
+
+@Composable
+fun ProductListScreen(listUiState: ProductListUiState, modifier: Modifier = Modifier) {
+    when (listUiState) {
+
+        ProductListUiState.Loading -> {
+            CircularProgressIndicator()
+        }
+
+        is ProductListUiState.Products -> {
+            Scaffold(
+                modifier = modifier
+                    .fillMaxSize(),
+                topBar = {
+                    ProductListTopBar(topBarTitle = "상품 목록", rightIcon = Icons.Filled.ShoppingCart)
+                },
+            ) { contentPadding ->
+                ProductListContents(
+                    modifier = Modifier
+                        .padding(contentPadding),
+                    productItems = listUiState.products
+                )
+            }
+        }
+
+        ProductListUiState.Error -> {
+            // Error 뷰
+        }
+
+        ProductListUiState.Empty -> {
+            // Empty 뷰
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListScreenPreview() {
+    val uiState = ProductListUiState.Products(lists)
+    ShoppingCartTheme {
+        ProductListScreen(uiState)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListScreenLoadingPreview() {
+    val uiState = ProductListUiState.Loading
+    ShoppingCartTheme {
+        ProductListScreen(uiState)
+    }
+}
+
+val lists = listOf(
+    Product(
+        name = "PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    ), Product(
+        name = "PET 보틀 - 정사각형 음료수",
+        imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+        price = "10000",
+        link = "상세로이동"
+    )
+)
