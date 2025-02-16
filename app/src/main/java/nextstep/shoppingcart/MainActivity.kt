0f838c5282be5a7e8b6744e091f07db30755ffcc
parkduksung <duksung1234@naver.com>
2025-02-16 09:48:39 +0900

    refactor: 현 상황에서의 불필요한 surface 제거.

--- app/src/main/java/nextstep/shoppingcart/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/MainActivity.kt
@@ -3,10 +3,6 @@ package nextstep.shoppingcart
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
 import nextstep.shoppingcart.designsystem.theme.ShoppingCartTheme
 import nextstep.shoppingcart.ui.list.ProductListScreen
 
@@ -15,13 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingCartTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    ProductListScreen()
-                }
+                ProductListScreen()
             }
         }
     }
