97906c2044edb25d37efa963b731c6e1a5a8f8c4
KimMooHyeon <dlfb77@gmail.com>
2025-02-17 02:00:27 +0900

    feat : 상품 목록 화면에서 사용되는 컴포넌트 추가

---
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/ProductListContents.kt
@@ -0,0 +1,28 @@
+package nextstep.shoppingcart.ui.theme.shopping.component
+
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.ui.theme.shopping.lists
+import nextstep.shoppingcart.ui.theme.shopping.model.Product
+
+@Composable
+fun ProductListContents(modifier: Modifier = Modifier, productItems: List<Product>) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(2),
+    ) {
+        items(productItems) { item ->
+            Product(item)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListContentsPreview() {
+    ProductListContents(productItems = lists)
+}
---
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/ProductListTopBar.kt
@@ -0,0 +1,71 @@
+package nextstep.shoppingcart.ui.theme.shopping.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+
+@Composable
+fun ProductListTopBar(
+    topBarTitle: String,
+    modifier: Modifier = Modifier,
+    leftIcon: ImageVector? = null,
+    leftIconContentDescription: String = "",
+    rightIcon: ImageVector? = null,
+    rightIconContentDescription: String = "",
+    onLeftIconClicked: () -> Unit = {},
+    onRightIconClicked: () -> Unit = {},
+    ) {
+    Box(
+        modifier = modifier
+            .height(64.dp)
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp),
+    ) {
+        Text(
+            text = topBarTitle,
+            fontSize = 22.sp,
+            modifier = Modifier.align(Alignment.Center),
+            textAlign = TextAlign.Center
+        )
+        leftIcon?.let { leftIcon ->
+            Icon(
+                imageVector = leftIcon,
+                contentDescription = leftIconContentDescription,
+                modifier = Modifier.align(Alignment.CenterStart).clickable {
+                    onLeftIconClicked()
+                }
+            )
+        }
+        rightIcon?.let { rightIcon ->
+            Icon(
+                imageVector = rightIcon, contentDescription = rightIconContentDescription,
+                modifier = Modifier.align(Alignment.CenterEnd).clickable {
+                    onRightIconClicked()
+                }
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListTopBarPreview() {
+    ShoppingCartTheme {
+        ProductListTopBar(topBarTitle = "상품 목록", rightIcon = Icons.Filled.ShoppingCart)
+    }
+}
