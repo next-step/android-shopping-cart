83f7c07b0b779185792bfb737cdad9bd84b4c5d1
parkduksung <duksung1234@naver.com>
2025-02-17 23:13:24 +0900

    feat - 제공해준 Item, Util 추가.

---
+++ app/src/main/java/nextstep/shoppingcart/util/CartUtil.kt
@@ -0,0 +1,41 @@
+package nextstep.shoppingcart.util
+
+import nextstep.shoppingcart.model.Cart
+import nextstep.shoppingcart.model.Product
+
+
+object CartUtil {
+    private val _items: MutableList<Cart> = mutableListOf()
+    val items: List<Cart> get() = _items.toList()
+
+    val totalPrice: Int get() = _items.sumOf { it.totalPrice }
+
+    fun addOne(product: Product): List<Cart> {
+        val item = _items.find { it.product == product }
+        if (item == null) {
+            _items.add(Cart(product, 1))
+        } else {
+            val index = _items.indexOf(item)
+            _items[index] = item.copy(count = item.count + 1)
+        }
+        return items
+    }
+
+    fun removeOne(product: Product): List<Cart> {
+        _items.find { it.product == product }
+            ?.let { item ->
+                if (item.count > 1) {
+                    val index = _items.indexOf(item)
+                    _items[index] = item.copy(count = item.count - 1)
+                } else {
+                    _items.remove(item)
+                }
+            }
+        return items
+    }
+
+    fun removeAll(product: Product): List<Cart> {
+        _items.removeAll { it.product == product }
+        return items
+    }
+}
