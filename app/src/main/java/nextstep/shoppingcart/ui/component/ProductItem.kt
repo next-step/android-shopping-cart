366af398f30b00db11a618e0e1d49f18e8104189
minoo <minwoo@dailyshot.co>
2025-02-18 02:27:54 +0900

    style: 상품 아이템 컴포넌트 > 상품명 ellipsize 옵션 추가

--- app/src/main/java/nextstep/shoppingcart/ui/component/ProductItem.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/component/ProductItem.kt
@@ -14,6 +14,8 @@ import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -55,17 +57,20 @@ fun ProductItem(
         ) {
             Text(
                 text = product.name,
-                modifier = modifier,
+                modifier = modifier.fillMaxWidth(),
                 color = Black,
                 fontSize = 14.sp,
                 fontWeight = FontWeight.W700,
                 fontFamily = FontFamily.SansSerif,
+                textAlign = TextAlign.Start,
                 lineHeight = 14.sp,
                 letterSpacing = 0.5.sp,
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
             )
             Text(
                 text = product.formattedPrice,
-                modifier = modifier,
+                modifier = modifier.fillMaxWidth(),
                 color = Black,
                 fontSize = 16.sp,
                 fontWeight = FontWeight.W400,
@@ -82,7 +87,7 @@ fun ProductItem(
 fun ProductItemPreview() {
     val dummyItem = Product(
         imageUrl = "https://picsum.photos/600/600",
-        name = "상품명",
+        name = "상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명상품명",
         price = 10000
     )
     ProductItem(
