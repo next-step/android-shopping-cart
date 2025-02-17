2c58f2395ec2e4f83b9441b4b508ce4051f5b914
Bandal <no1msh1217@gmail.com>
2025-02-17 15:29:56 +0900

    refactor: 장바구니 품목 객체에 count 기본값 설정

--- app/src/main/java/nextstep/shoppingcart/domain/model/CartItem.kt
+++ app/src/main/java/nextstep/shoppingcart/domain/model/CartItem.kt
@@ -2,7 +2,7 @@ package nextstep.shoppingcart.domain.model
 
 data class CartItem(
     val product: Product,
-    val count: Count,
+    val count: Count = Count.ONE,
 ) {
     val totalPrice: Int get() = product.price.value * count.value
 }
