32bd141eaaa599a4a7e71fe0443c7bde91a5989a
minoo <minwoo@dailyshot.co>
2025-02-18 02:42:22 +0900

    refactor: dummy 데이터의 이미지 주소 변경

--- app/src/main/java/nextstep/shoppingcart/ui/screen/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/screen/ProductListScreen.kt
@@ -6,18 +6,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.R
 import nextstep.shoppingcart.data.Product
 import nextstep.shoppingcart.ui.component.ProductLazyGrid
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
-import nextstep.shoppingcart.R
 
 @Composable
 fun ProductScreen() {
     val appBarTitle = stringResource(R.string.title_product_list)
-    val products: List<Product> = List(20) { _ ->
+    val products: List<Product> = List(20) { index ->
         Product(
-            imageUrl = "https://picsum.photos/600/600",
-            name = "상품명",
+            imageUrl = "https://picsum.photos/600/600?random=$index",
+            name = "상품명 $index",
             price = 10000
         )
     }
