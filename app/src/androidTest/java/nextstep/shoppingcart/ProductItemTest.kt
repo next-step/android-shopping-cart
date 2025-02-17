e7bcdea04a8b6edaeb6de3602502977a3f567ad7
parkduksung <duksung1234@naver.com>
2025-02-17 18:24:25 +0900

    refactor - 불필요한 테스트케이스 제거.

--- app/src/androidTest/java/nextstep/shoppingcart/ProductItemTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/ProductItemTest.kt
@@ -1,10 +1,6 @@
 package nextstep.shoppingcart
 
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
-import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.list.component.ProductItem
@@ -52,30 +48,5 @@ class ProductItemTest {
 
         //then
         composeTestRule.onNodeWithText("12000").assertExists()
-    }
-
-    @Test
-    fun should_show_correct_product_image_when_create_product_item_component() {
-        //given
-        val mockProduct = Product(
-            id = 5,
-            name = "test55",
-            price = 12000,
-            imageUrl = "https://search.pstatic.net/common/?src=http%3A%2F%2Fshop1.phinf.naver.net%2F20181030_239%2Fcomscience1_1540871845728YC8OA_JPEG%2F01.jpg&type=a340",
-        )
-
-        //when
-        composeTestRule.setContent {
-            ProductItem(
-                modifier = Modifier.testTag("productImage"),
-                item = mockProduct
-            )
-        }
-
-        //then
-        composeTestRule
-            .onNodeWithTag("productImage")
-            .assertExists()
-            .assertIsDisplayed()
     }
 }
