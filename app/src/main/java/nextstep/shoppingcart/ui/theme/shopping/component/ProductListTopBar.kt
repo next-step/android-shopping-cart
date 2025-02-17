bf7f090119b62e7f5e5f5f6b26db6c9188e399e5
KimMooHyeon <dlfb77@gmail.com>
2025-02-18 00:15:25 +0900

    refactor : ProductListTopBar 컴포넌트 내부구현 CenterAlignedTopAppBar로 변경

--- app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/ProductListTopBar.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/ProductListTopBar.kt
@@ -1,69 +1,64 @@
 package nextstep.shoppingcart.ui.theme.shopping.component
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProductListTopBar(
     topBarTitle: String,
-    modifier: Modifier = Modifier,
     leftIcon: ImageVector? = null,
     leftIconContentDescription: String = "",
+    onLeftIconClicked: () -> Unit = {},
     rightIcon: ImageVector? = null,
     rightIconContentDescription: String = "",
-    onLeftIconClicked: () -> Unit = {},
     onRightIconClicked: () -> Unit = {},
 ) {
-    Box(
-        modifier = modifier
-            .height(64.dp)
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
-    ) {
-        Text(
-            text = topBarTitle,
-            fontSize = 22.sp,
-            modifier = Modifier.align(Alignment.Center),
-            textAlign = TextAlign.Center
-        )
-        leftIcon?.let { leftIcon ->
-            Icon(
-                imageVector = leftIcon,
-                contentDescription = leftIconContentDescription,
-                modifier = Modifier
-                    .align(Alignment.CenterStart)
-                    .clickable {
-                        onLeftIconClicked()
-                    }
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = topBarTitle,
+                fontSize = 22.sp,
+                textAlign = TextAlign.Center
             )
+        },
+        navigationIcon = {
+            leftIcon?.let { icon ->
+                Icon(
+                    imageVector = icon,
+                    contentDescription = leftIconContentDescription,
+                    modifier = Modifier
+                        .clickable {
+                            onLeftIconClicked()
+                        }
+                )
+            }
+        },
+        actions = {
+            rightIcon?.let { icon ->
+                Icon(
+                    imageVector = icon,
+                    contentDescription = rightIconContentDescription,
+                    modifier = Modifier
+                        .clickable {
+                            onRightIconClicked()
+                        }
+                )
+            }
         }
-        rightIcon?.let { rightIcon ->
-            Icon(
-                imageVector = rightIcon, contentDescription = rightIconContentDescription,
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .clickable {
-                        onRightIconClicked()
-                    }
-            )
-        }
-    }
+    )
 }
 
 @Preview(showBackground = true)
