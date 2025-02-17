976142abc1c3a1df59316ac4b1fc52989a351090
김무현 <2230198@ad.h-firework.com>
2025-02-17 13:47:50 +0900

    fix : 디자인 가이드에 맞게 수평 padding 값 추가

--- app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/Product.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/theme/shopping/component/Product.kt
@@ -2,12 +2,15 @@ package nextstep.shoppingcart.ui.theme.shopping.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import nextstep.shoppingcart.ui.theme.shopping.model.Product
 
@@ -18,7 +21,8 @@ fun Product(product: Product, modifier: Modifier = Modifier) {
             model = product.imageUrl,
             contentDescription = null,
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth().padding(horizontal = 6.dp),
+            contentScale = ContentScale.Crop
         )
         Text(
             product.name,
