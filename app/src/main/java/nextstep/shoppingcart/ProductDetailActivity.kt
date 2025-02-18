98b32a4ac9d429c65fbbc8a2a5baec0cccf39b68
김무현 <2230198@ad.h-firework.com>
2025-02-18 19:44:27 +0900

    feat : 상품 상세화면 구성 및 화면이동 함수 전달

--- app/src/main/java/nextstep/shoppingcart/ProductDetailActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/ProductDetailActivity.kt
@@ -1,24 +1,60 @@
 package nextstep.shoppingcart
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import nextstep.shoppingcart.model.detail.ProductDetailUiState
+import nextstep.shoppingcart.model.productTestDataList
+import nextstep.shoppingcart.ui.ProductDetailScreen
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class ProductDetailActivity : ComponentActivity() {
+    private val productId: String by lazy {
+        intent.getStringExtra("productId")
+            ?: throw IllegalArgumentException("productId is required")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var product: ProductDetailUiState by remember { mutableStateOf(ProductDetailUiState.Loading) }
+
+            LaunchedEffect(Unit) {
+                product = getProductDetails(productId)
+            }
+
             ShoppingCartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    innerPadding
-                }
+                ProductDetailScreen(
+                    uiState = product,
+                    navigateToCart = {
+                        //TODO step3 장바구니 담기 구현
+                        navigateToCart()
+                    },
+                    onBackButtonClick = { onBackPressedDispatcher.onBackPressed() })
             }
         }
+    }
+
+    private fun navigateToCart() {
+        val intent = Intent(this, CartActivity::class.java)
+        startActivity(intent)
+    }
+}
+
+fun getProductDetails(productId: String): ProductDetailUiState {
+    return try {
+        productTestDataList.find { it.productId == productId }?.let {
+            ProductDetailUiState.ProductDetail(it)
+        } ?: ProductDetailUiState.Empty
+    } catch (t: Throwable) {
+        ProductDetailUiState.Error
     }
 }
