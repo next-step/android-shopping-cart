ec45458c8a738399ff525893da612955d5e1c4ad
김무현 <2230198@ad.h-firework.com>
2025-02-18 20:00:15 +0900

    feat : 장바구니 임시화면 적용

--- app/src/main/java/nextstep/shoppingcart/CartActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/CartActivity.kt
@@ -4,19 +4,46 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class CartActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ShoppingCartTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    innerPadding
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = { Text(text = "장바구니") },
+                            navigationIcon = {
+                                Icon(
+                                    modifier = Modifier
+                                        .clickable { onBackPressedDispatcher.onBackPressed() }
+                                        .padding(16.dp),
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = "장바구니 뒤로가기 버튼"
+                                )
+                            },
+                        )
+                    },
+                ) { contentPadding ->
+                    contentPadding
                 }
             }
         }
