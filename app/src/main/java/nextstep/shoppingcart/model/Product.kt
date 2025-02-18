1d462cd645d57eeaffefc8a4e51176d1ff71a987
김무현 <2230198@ad.h-firework.com>
2025-02-18 19:53:18 +0900

    chore : 상품 ID 기반 데이터 전달이 정상적인지 판단 용이를 위한 테스트 데이터값 변경

--- app/src/main/java/nextstep/shoppingcart/model/Product.kt
+++ app/src/main/java/nextstep/shoppingcart/model/Product.kt
@@ -22,9 +22,9 @@ private fun getProductsTestData(): List<Product> {
     for (i in 1..30) {
         list.add(
             Product(
-                name = "PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
+                name = "[${i}] PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
                 imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
-                price = "10000",
+                price = "10000${i}",
                 productId = "id${i}"
             )
         )
