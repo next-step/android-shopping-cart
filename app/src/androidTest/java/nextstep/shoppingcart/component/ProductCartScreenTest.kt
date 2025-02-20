2d61906c6d46aa037eff6564857e817b302c53e0
raindragonn <raindragonn@gmail.com>
2025-02-20 10:49:15 +0900

    test: 장바구니화면 테스트 변경

--- app/src/androidTest/java/nextstep/shoppingcart/component/ProductCartScreenTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/component/ProductCartScreenTest.kt
@@ -1,19 +1,16 @@
 package nextstep.shoppingcart.component
 
-import androidx.compose.ui.test.assertCountEquals
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertIsEnabled
-import androidx.compose.ui.test.assertIsNotDisplayed
-import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onAllNodesWithTag
+import androidx.compose.ui.test.onFirst
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
+import nextstep.shoppingcart.data.model.Cart
+import nextstep.shoppingcart.data.model.CartItem
 import nextstep.shoppingcart.data.model.Product
-import nextstep.shoppingcart.data.repository.CartRepository
 import nextstep.shoppingcart.ui.ProductCartScreen
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,169 +19,131 @@ class ProductCartScreenTest {
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    private val orderButtonNode
-        get() = composeTestRule
-            .onNodeWithText("주문하기", substring = true)
-
-    private val cartItemNode
-        get() = composeTestRule
-            .onNodeWithTag("ProductCartItem")
-
-    private val decreaseButtonNode
-        get() = composeTestRule
-            .onNodeWithTag("상품1_decrease_button")
-
-    private val product =
-        Product(
-            id = 1,
-            imageUrl = "",
-            name = "상품1",
-            price = 2000,
+    private val cart = Cart(
+        items = listOf(
+            CartItem(
+                product = Product(
+                    id = 1,
+                    imageUrl = "",
+                    name = "상품1",
+                    price = 2000,
+                ),
+                count = 3
+            ),
+            CartItem(
+                product = Product(
+                    id = 2,
+                    imageUrl = "",
+                    name = "상품2",
+                    price = 1000,
+                ),
+                count = 1
+            )
         )
-
-    @Before
-    fun setUp() {
-        CartRepository.reset()
-    }
+    )
 
     @Test
-    fun `담긴_상품_가격의_총합이_노출된다`() {
-        CartRepository.addOne(product)
-        CartRepository.addOne(product)
-
+    fun `뒤로가기_버튼_클릭_이벤트를_받는다`() {
+        var clicked = false
         composeTestRule
             .setContent {
                 ProductCartScreen(
-                    onBackButtonClick = { }
+                    cartItems = cart.items,
+                    totalPrice = cart.totalPrice,
+                    orderButtonEnabled = true,
+                    onBackButtonClick = { clicked = true },
+                    onRemoveClick = { },
+                    onIncreaseClick = { },
+                    onDecreaseClick = { },
+                    onOrderClick = { },
                 )
             }
 
         composeTestRule
-            .onNodeWithText("주문하기(4,000원)")
+            .onNodeWithTag("back_navigation")
+            .performClick()
+
+        assert(clicked == true)
+    }
+
+
+    @Test
+    fun `담긴_상품_가격의_총합이_주문하기_버튼에_노출된다`() {
+        val totalPrice = cart.totalPrice
+
+        composeTestRule
+            .setContent {
+                ProductCartScreen(
+                    cartItems = cart.items,
+                    totalPrice = totalPrice,
+                    orderButtonEnabled = true,
+                    onBackButtonClick = { },
+                    onRemoveClick = { },
+                    onIncreaseClick = { },
+                    onDecreaseClick = { },
+                    onOrderClick = { },
+                )
+            }
+
+        composeTestRule
+            .onNodeWithText("7,000원", substring = true)
             .assertIsDisplayed()
     }
 
     @Test
-    fun `담긴_상품을_제거할_수_있다`() {
-        CartRepository.addOne(product)
+    fun `제거_버튼을_클릭한_상품의_정보를_가져온다`() {
+        var removeProduct: Product? = null
 
         composeTestRule
             .setContent {
                 ProductCartScreen(
-                    onBackButtonClick = { }
+                    cartItems = cart.items,
+                    totalPrice = cart.totalPrice,
+                    orderButtonEnabled = true,
+                    onBackButtonClick = { },
+                    onRemoveClick = { removeProduct = it },
+                    onIncreaseClick = { },
+                    onDecreaseClick = { },
+                    onOrderClick = { },
                 )
             }
 
         composeTestRule
-            .onNodeWithTag("상품1_remove_button")
+            .onAllNodesWithTag("remove_button")
+            .onFirst()
             .performClick()
 
-        cartItemNode
-            .assertIsNotDisplayed()
+        assert(removeProduct?.name == "상품1")
     }
 
     @Test
-    fun `담긴_상품의_수량을_증가시키면_상품_가격에_반영된다`() {
-        CartRepository.addOne(product)
+    fun `수량조절_버튼을_클릭한_상품의_정보를_가져온다`() {
+        var increaseProduct: Product? = null
+        var decreaseProduct: Product? = null
 
         composeTestRule
             .setContent {
                 ProductCartScreen(
-                    onBackButtonClick = { }
+                    cartItems = cart.items,
+                    totalPrice = cart.totalPrice,
+                    orderButtonEnabled = true,
+                    onBackButtonClick = { },
+                    onRemoveClick = { },
+                    onIncreaseClick = { increaseProduct = it },
+                    onDecreaseClick = { decreaseProduct = it },
+                    onOrderClick = { },
                 )
             }
-
         composeTestRule
-            .onNodeWithTag("상품1_increase_button")
-            .performClick()
-            .performClick()
-
-        composeTestRule
-            .onNodeWithText("주문하기(6,000원)")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun `담긴_상품의_수량을_감소시키면_상품_가격에_반영된다`() {
-        CartRepository.addOne(product)
-        CartRepository.addOne(product)
-
-        composeTestRule
-            .setContent {
-                ProductCartScreen(
-                    onBackButtonClick = { }
-                )
-            }
-
-        decreaseButtonNode
+            .onAllNodesWithTag("increase_button")
+            .onFirst()
             .performClick()
 
         composeTestRule
-            .onNodeWithText("주문하기(2,000원)")
-            .assertIsDisplayed()
-    }
-
-    @Test
-    fun `담긴_상품의_수량을_1보다_적게_하면_상품이_삭제된다`() {
-        CartRepository.addOne(product)
-
-        composeTestRule
-            .setContent {
-                ProductCartScreen(
-                    onBackButtonClick = { }
-                )
-            }
-
-        decreaseButtonNode
+            .onAllNodesWithTag("decrease_button")[1]
             .performClick()
 
-        cartItemNode
-            .assertIsNotDisplayed()
-    }
-
-    @Test
-    fun `상품의_종류별로_항목이_생긴다`() {
-        CartRepository.addOne(Product(id = 1, "", "상품1", 1000))
-        CartRepository.addOne(Product(id = 2, "", "상품2", 1000))
-        CartRepository.addOne(Product(id = 3, "", "상품3", 1000))
-
-        composeTestRule
-            .setContent {
-                ProductCartScreen(
-                    onBackButtonClick = { }
-                )
-            }
-
-        composeTestRule
-            .onAllNodesWithTag("ProductCartItem")
-            .assertCountEquals(3)
-    }
-
-    @Test
-    fun `담겨진_상품이_없으면_주문하기_버튼이_비활성화_된다`() {
-        composeTestRule
-            .setContent {
-                ProductCartScreen(
-                    onBackButtonClick = { }
-                )
-            }
-
-        orderButtonNode
-            .assertIsNotEnabled()
-    }
-
-    @Test
-    fun `담겨진_상품이_있으면_주문하기_버튼이_활성화_된다`() {
-        CartRepository.addOne(product)
-
-        composeTestRule
-            .setContent {
-                ProductCartScreen(
-                    onBackButtonClick = { }
-                )
-            }
-
-        orderButtonNode
-            .assertIsEnabled()
+        assert(increaseProduct?.name == "상품1")
+        assert(decreaseProduct?.name == "상품2")
     }
 }
