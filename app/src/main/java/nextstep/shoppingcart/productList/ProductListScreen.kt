17b7a76e3a84df190b7f3f2abd363245706fd55f
YoonJam <yhw1904@gmail.com>
2025-02-23 23:32:00 +0900

    style: 배경 색상 white 로 변경

--- app/src/main/java/nextstep/shoppingcart/productList/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/productList/ProductListScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.data.Product
 
@@ -20,6 +21,7 @@ fun ProductListScreen(
     }
 
     Scaffold(
+        containerColor = Color.White,
         topBar = { ProductListTopAppBar() }
     ) { innerPadding ->
         ProductList(
