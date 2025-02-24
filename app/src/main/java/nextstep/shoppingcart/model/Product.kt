0379d88231804479f4e6661f35159a7e1c842f2c
김무현 <2230198@ad.h-firework.com>
2025-02-24 14:27:08 +0900

    feat : 장바구니 데이터 적재 및 관리 object 추가

--- app/src/main/java/nextstep/shoppingcart/model/Product.kt
+++ app/src/main/java/nextstep/shoppingcart/model/Product.kt
@@ -5,29 +5,25 @@ import java.text.DecimalFormat
 data class Product(
     val name: String,
     val imageUrl: String,
-    val price: String,
+    val price: Int,
     val productId: String
 ) {
     val formattedPrice: String
-        get() = numberMoneyFormat.format(price.toInt()) + "원"
+        get() = numberMoneyFormat.format(price) + "원"
 
 }
 
-val numberMoneyFormat = DecimalFormat("#,###")
+private val numberMoneyFormat = DecimalFormat("#,###")
 
-var productTestDataList = getProductsTestData()
+val productTestDataList = getProductsTestData()
 
 private fun getProductsTestData(): List<Product> {
-    val list = mutableListOf<Product>()
-    for (i in 1..30) {
-        list.add(
-            Product(
-                name = "[${i}] PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
-                imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
-                price = "10000${i}",
-                productId = "id${i}"
-            )
+    return List(30) { i ->
+        Product(
+            name = "[${i}] PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
+            imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+            price = 10000 + i,
+            productId = "id${i}"
         )
     }
-    return list.toList()
 }
