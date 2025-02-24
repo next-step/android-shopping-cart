36029b93502955035658ad82c52a29c4f0fcaedb
김무현 <2230198@ad.h-firework.com>
2025-02-24 14:39:08 +0900

    refactor : 필수 파라미터 변경에 따른 ProductDetailScreen Preview 변경

--- app/src/main/java/nextstep/shoppingcart/productdetail/component/ProductDetailContent.kt
+++ app/src/main/java/nextstep/shoppingcart/productdetail/component/ProductDetailContent.kt
@@ -55,7 +55,7 @@ private fun ProductDetailContentPreview() {
         product = Product(
             name = "PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
             imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
-            price = "10000",
+            price = 10000,
             productId = "id1"
         ),
     )
