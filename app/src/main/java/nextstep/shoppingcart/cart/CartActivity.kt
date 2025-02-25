dd656c41fb172a430d183ee0133d977383663778
김무현 <2230198@ad.h-firework.com>
2025-02-25 15:17:24 +0900

    feat : 장바구니 화면 구현 - 상품 수량 조절 - 1보다 적을경우 상품 제거 - 담긴 상품의 총합 가격 하단에 주문하기 버튼에 표시

--- app/src/main/java/nextstep/shoppingcart/cart/CartActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/cart/CartActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import nextstep.shoppingcart.cart.model.CartViewModel
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class CartActivity : ComponentActivity() {
-
+    private val viewModel: CartViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             ShoppingCartTheme {
-                CartScreen(onBackButtonClick = { onBackPressedDispatcher.onBackPressed() })
+                CartScreen(
+                    onBackButtonClick = { onBackPressedDispatcher.onBackPressed() },
+                    viewModel = viewModel
+                )
             }
         }
     }
--- app/src/main/java/nextstep/shoppingcart/cart/CartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/cart/CartScreen.kt
@@ -4,27 +4,42 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import nextstep.shoppingcart.Cart
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import nextstep.shoppingcart.cart.component.CartContent
-import nextstep.shoppingcart.model.CartItem
+import nextstep.shoppingcart.cart.model.CartUiState
+import nextstep.shoppingcart.cart.model.CartViewModel
+import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.component.ProductBackButtonTopBar
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 @Composable
-fun CartScreen(onBackButtonClick: () -> Unit, modifier: Modifier = Modifier) {
-    val cartItem by remember { mutableStateOf(Cart.items) }
-    CartScreen(cartItem = cartItem, onBackButtonClick = { onBackButtonClick() })
+fun CartScreen(
+    viewModel: CartViewModel,
+    onBackButtonClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+    CartScreen(
+        uiState = uiState.value,
+        onBackButtonClick = onBackButtonClick,
+        onAddOneToCart = { product -> viewModel.addOne(product) },
+        onRemoveOneFromCart = { product -> viewModel.removeOne(product) },
+        onClearCartItem = { product -> viewModel.removeAll(product) },
+        onOrderButtonClick = {},
+        modifier = modifier
+    )
 }
 
 @Composable
 private fun CartScreen(
     onBackButtonClick: () -> Unit,
-    cartItem: List<CartItem>,
+    uiState: CartUiState,
+    onAddOneToCart: (Product) -> Unit,
+    onRemoveOneFromCart: (Product) -> Unit,
+    onClearCartItem: (Product) -> Unit,
+    onOrderButtonClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Scaffold(
@@ -38,8 +53,17 @@ private fun CartScreen(
             )
         },
     ) { contentPadding ->
-        //  CartScreen(modifier = Modifier.padding(contentPadding), cartItem)
-        CartContent(modifier = modifier.padding(contentPadding), cartItem = cartItem)
+        CartContent(
+            modifier = modifier
+                .padding(contentPadding)
+                .fillMaxSize(),
+            cartItem = uiState.cartItems,
+            onAddOneToCart = onAddOneToCart,
+            onRemoveOneFromCart = onRemoveOneFromCart,
+            onClearCartItem = onClearCartItem,
+            totalPrice = uiState.totalPrice,
+            onOrderButtonClick = onOrderButtonClick
+        )
     }
 }
 
@@ -47,6 +71,9 @@ private fun CartScreen(
 @Composable
 private fun CartScreenPreview() {
     ShoppingCartTheme {
-        CartScreen({})
+        CartScreen(
+            onBackButtonClick = {},
+            viewModel = CartViewModel(repository = nextstep.shoppingcart.Cart),
+        )
     }
 }
