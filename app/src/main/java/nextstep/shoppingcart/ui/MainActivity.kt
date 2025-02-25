a9db2c04dda91a92bfe5ce2625243a3e07cabf61
ver <ver@sendy.ai>
2025-02-26 00:07:31 +0900

    feat: 상품 목록에서 장바구니에 담을/담긴 상품의 수량을 조절할 수 있다

--- app/src/main/java/nextstep/shoppingcart/ui/MainActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/MainActivity.kt
@@ -4,7 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import nextstep.shoppingcart.FakeProductsRepository
+import nextstep.shoppingcart.model.Cart
 import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.productdetail.ProductDetailActivity
 import nextstep.shoppingcart.ui.products.ProductsScreen
@@ -17,10 +23,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingCartTheme {
                 enableEdgeToEdge()
+                var cartItems by remember { mutableStateOf(Cart.items) }
+
                 ProductsScreen(
                     products = FakeProductsRepository.getProducts(),
                     onProductClick = { product -> handleProductClick(product) },
                     onShoppingCartActionClick = ::handleShoppingCartActionClick,
+                    cartItems = cartItems,
+                    onProductAddClick = { product ->
+                        Cart.addOne(product)
+                        cartItems = Cart.items
+                    },
+                    onProductRemoveClick = {
+                        Cart.removeOne(it)
+                        cartItems = Cart.items
+                    }
                 )
             }
         }
--- app/src/main/java/nextstep/shoppingcart/ui/components/ShoppingCartCounter.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/components/ShoppingCartCounter.kt
@@ -1,5 +1,6 @@
 package nextstep.shoppingcart.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
@@ -11,6 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,7 +27,7 @@ fun ShoppingCartCounter(
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier.clip(RoundedCornerShape(4.dp)),
+        modifier = modifier.clip(shape = RoundedCornerShape(4.dp)),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = CenterVertically
     ) {
