6f242637c0a91bfec605e3b6a262f47db685349d
YoonJam <yhw1904@gmail.com>
2025-02-25 23:53:47 +0900

    feat: CartScreen composable 함수 추가

--- app/src/main/java/nextstep/shoppingcart/cart/CartActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/cart/CartActivity.kt
@@ -1,8 +1,12 @@
 package nextstep.shoppingcart.cart
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class CartActivity : ComponentActivity() {
@@ -10,8 +14,26 @@ class CartActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingCartTheme {
-
+                CartScreen(
+                    onBackButtonClick = {
+                        finish()
+                    }
+                )
             }
         }
+    }
+
+    companion object {
+        fun start(context: Context) {
+            context.startActivity(Intent(context, CartActivity::class.java))
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ProductDetailActivityPreview() {
+    ShoppingCartTheme {
+        CartScreen()
     }
 }
---
+++ app/src/main/java/nextstep/shoppingcart/cart/CartScreen.kt
@@ -0,0 +1,35 @@
+package nextstep.shoppingcart.cart
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun CartScreen(
+    onBackButtonClick: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CartTopAppBar(onBackButtonClick = onBackButtonClick)
+        },
+        containerColor = Color.White
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun CartScreenPreview() {
+    CartScreen()
+}
