1a8c0cc2dc102b45e18169f0ebd77f75f9049d6d
YoonJam <yhw1904@gmail.com>
2025-02-25 23:46:46 +0900

    feat: 장바구니 activity 추가

---
+++ app/src/main/java/nextstep/shoppingcart/cart/CartActivity.kt
@@ -0,0 +1,17 @@
+package nextstep.shoppingcart.cart
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
+
+class CartActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShoppingCartTheme {
+
+            }
+        }
+    }
+}
