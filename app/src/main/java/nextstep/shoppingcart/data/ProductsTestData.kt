2959b21de7eaab596e102132057ee14e0ffa4f03
김무현 <2230198@ad.h-firework.com>
2025-02-25 16:46:16 +0900

    chore : 테스트 데이터 image URL 수정

--- app/src/main/java/nextstep/shoppingcart/data/ProductsTestData.kt
+++ app/src/main/java/nextstep/shoppingcart/data/ProductsTestData.kt
@@ -6,7 +6,7 @@ object ProductsTestData {
     val productTestDataList = List(30) { i ->
         Product(
             name = "[${i}] PET 보틀 - 음료수,정사각형 음료수,정사각형 음료수,정사각형 음료수",
-            imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
+            imageUrl = "https://i.namu.wiki/i/Ert2K83rRrg_S7Z-T1wpr2yu2Qp0cAF4XHkah-rAjQaJP9YzTQvHGQ90p5cgs2OEEJIDHBXkHVYL3AuJqaX9xGN91bOoCVfIHyZxMZ7ml6ZEvNRB9ibxNSml6VAno4o6JeOuPEWOGg1cw3N5wIB-xw.webp",
             price = 10000 + i,
             productId = "id${i}"
         )
