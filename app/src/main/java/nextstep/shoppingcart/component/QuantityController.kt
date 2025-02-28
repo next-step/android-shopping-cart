2a62a88d49a900b34e44822b605649694de02339
YoonJam <yhw1904@gmail.com>
2025-02-28 22:58:41 +0900

    feat: 수량 조절 UI 컴포저블 추가

---
+++ app/src/main/java/nextstep/shoppingcart/component/QuantityController.kt
@@ -0,0 +1,77 @@
+package nextstep.shoppingcart.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun QuantityController(
+    count: Int,
+    onMinusClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+    ) {
+        TextButton(
+            onClick = onMinusClick,
+            modifier = Modifier.size(42.dp),
+            colors = ButtonDefaults.textButtonColors(
+                contentColor = Color.Black,
+            ),
+        ) {
+            Text(
+                text = "-",
+            )
+        }
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.size(42.dp),
+        ) {
+            Text(
+                text = count.toString(),
+            )
+        }
+        TextButton(
+            onClick = onPlusClick,
+            modifier = Modifier.size(42.dp),
+            colors = ButtonDefaults.textButtonColors(
+                contentColor = Color.Black,
+            ),
+        ) {
+            Text(
+                text = "+",
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun QuantityControllerPreview() {
+    var count by remember { mutableIntStateOf(0) }
+
+    QuantityController(
+        count = count,
+        onMinusClick = {
+            count--
+        },
+        onPlusClick = {
+            count++
+        },
+    )
+}
