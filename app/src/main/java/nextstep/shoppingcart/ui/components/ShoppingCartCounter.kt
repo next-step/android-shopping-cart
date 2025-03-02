f9cdf6a5a00c8aaf2dd719451baa0882f0b138e4
ver <ver@sendy.ai>
2025-03-02 10:31:33 +0900

    fix: ShoppingCartCounter 높이를 가이드에 맞춘다

--- app/src/main/java/nextstep/shoppingcart/ui/components/ShoppingCartCounter.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/components/ShoppingCartCounter.kt
@@ -1,8 +1,10 @@
 package nextstep.shoppingcart.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
@@ -11,8 +13,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.R
@@ -23,10 +27,11 @@ fun ShoppingCartCounter(
     counter: CartCount,
     onAddClick: () -> Unit,
     onRemoveClick: () -> Unit,
+    iconSize: Dp = 42.dp,
     modifier: Modifier = Modifier
 ) {
     Row(
-        modifier = modifier.clip(shape = RoundedCornerShape(4.dp)),
+        modifier = modifier.background(shape = RoundedCornerShape(4.dp), color = Color.White),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = CenterVertically
     ) {
@@ -35,13 +40,14 @@ fun ShoppingCartCounter(
             icon = painterResource(id = R.drawable.ic_remove),
             onClick = onRemoveClick,
             contentDescription = "빼기",
+            modifier = Modifier.size(iconSize)
         )
 
         Text(
             text = counter.value.toString(),
             fontSize = 22.sp,
             modifier = Modifier
-                .size(42.dp)
+                .size(iconSize)
                 .wrapContentSize(Alignment.Center)
         )
 
@@ -49,12 +55,13 @@ fun ShoppingCartCounter(
             icon = painterResource(id = R.drawable.ic_add),
             onClick = onAddClick,
             contentDescription = "더하기",
+            modifier = Modifier.size(iconSize)
         )
     }
 }
 
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 private fun ShoppingCartCounterPreview() {
     ShoppingCartCounter(
