725c508d3e8d251151328166f8ef1936a894e426
DoTheBestMayB <send1684@gmail.com>
2025-03-06 02:45:12 +0900

    feat: + 만 존재하는 수량 조절 Composable 구현

--- app/src/main/java/nextstep/shoppingcart/ui/designsystem/QuantityHandler.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/designsystem/QuantityHandler.kt
@@ -1,9 +1,12 @@
 package nextstep.shoppingcart.ui.designsystem
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Icon
@@ -12,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -50,6 +54,38 @@ fun QuantityHandler(
                 contentDescription = stringResource(R.string.quantity_handler_increase),
             )
         }
+    }
+}
+
+@Composable
+fun QuantityHandlerOnlyPlus(
+    onIncreaseQuantityClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = Color.White,
+                shape = CircleShape
+            )
+    ) {
+        IconButton(
+            onClick = onIncreaseQuantityClick
+        ) {
+            Icon(imageVector = Icons.Default.Add, contentDescription = null)
+        }
+    }
+}
+
+@Preview(
+    showBackground = true,
+)
+@Composable
+private fun QuantityHandlerOnlyPlusPreview() {
+    ShoppingCartTheme {
+        QuantityHandlerOnlyPlus(
+            onIncreaseQuantityClick = {},
+        )
     }
 }
 
