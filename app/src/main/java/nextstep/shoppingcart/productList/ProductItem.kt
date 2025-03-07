4a5bc92d19f96771817b4d6d618b7171080fdba7
YoonJam <yhw1904@gmail.com>
2025-03-07 16:04:39 +0900

    refactor: count 조건에 의해 수량 조절 버튼이 2가지 형태로 보이도록 변경

--- app/src/main/java/nextstep/shoppingcart/productList/ProductItem.kt
+++ app/src/main/java/nextstep/shoppingcart/productList/ProductItem.kt
@@ -1,21 +1,33 @@
 package nextstep.shoppingcart.productList
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import nextstep.shoppingcart.component.ProductImage
 import nextstep.shoppingcart.component.ProductName
 import nextstep.shoppingcart.component.ProductPrice
+import nextstep.shoppingcart.component.QuantityController
 import nextstep.shoppingcart.data.DummyProduct
 import nextstep.shoppingcart.data.Product
 
@@ -23,8 +35,11 @@ import nextstep.shoppingcart.data.Product
 @Composable
 fun ProductItem(
     product: Product,
+    count: Int,
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
+    onPlusClick: () -> Unit,
+    onMinusClick: () -> Unit,
 ) {
     Surface(
         modifier = modifier,
@@ -32,12 +47,45 @@ fun ProductItem(
         onClick = onClick,
     ) {
         Column {
-            ProductImage(
-                imageUrl = product.imageUrl,
-                ratio = (156.0f / 158),
-                modifier = Modifier
-                    .fillMaxWidth(),
-            )
+            Box {
+                ProductImage(
+                    imageUrl = product.imageUrl,
+                    ratio = (156.0f / 158),
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                )
+                if (count > 0) {
+                    QuantityController(
+                        count = count,
+                        onMinusClick = onPlusClick,
+                        onPlusClick = onMinusClick,
+                        modifier = Modifier
+                            .align(Alignment.BottomCenter)
+                            .padding(bottom = 12.dp)
+                            .background(
+                                shape = RoundedCornerShape(corner = CornerSize(5.dp)),
+                                color = Color.White
+                            )
+                    )
+                } else {
+                    IconButton(
+                        onClick = onPlusClick,
+                        modifier = Modifier
+                            .padding(bottom = 12.dp, end = 12.dp)
+                            .clip(shape = CircleShape)
+                            .size(42.dp)
+                            .background(color = Color.White)
+                            .align(Alignment.BottomEnd),
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(24.dp),
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "product_add_button",
+                            tint = Color.Black,
+                        )
+                    }
+                }
+            }
             ProductName(
                 name = product.name,
                 modifier = Modifier
@@ -51,19 +99,28 @@ fun ProductItem(
                 modifier = Modifier
                     .padding(horizontal = 4.dp)
                     .fillMaxWidth(),
-                fontSize = 16.sp
+                fontSize = 16.sp,
             )
         }
     }
 }
 
+private class ProductItemPreviewParameterProvider : PreviewParameterProvider<Int> {
+    override val values = sequenceOf(0, 1)
+}
+
 @Preview(showBackground = true)
 @Composable
-private fun ProductItemPreview() {
+private fun ProductItemPreview(
+    @PreviewParameter(ProductItemPreviewParameterProvider::class) count: Int,
+) {
     val product = DummyProduct.product1
 
     ProductItem(
         product = product,
+        count = count,
         onClick = {},
+        onPlusClick = {},
+        onMinusClick = {},
     )
 }
