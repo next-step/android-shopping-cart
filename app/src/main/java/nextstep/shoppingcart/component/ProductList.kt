11c239625efabdaa928cedfdb4293a9d9d9b9152
hyemdooly <hyemdooly@gmail.com>
2025-03-08 02:35:57 +0900

    feat: 리스트 뷰 구현

---
+++ app/src/main/java/nextstep/shoppingcart/component/ProductList.kt
@@ -0,0 +1,47 @@
+package nextstep.shoppingcart.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.shoppingcart.model.Product
+
+@Composable
+fun ProductList(
+    products: List<Product>,
+    modifier: Modifier = Modifier
+) {
+    LazyVerticalGrid(
+        modifier = modifier,
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 18.dp, vertical = 13.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+    ) {
+        items(items = products, key = { it.id }) {
+            ProductListItem(
+                product = it,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListPreview() {
+    ProductList(
+        products = List(10) {
+            Product(
+                id = it,
+                imageUrl = "https://picsum.photos/id/1/300/300",
+                name = "PET보틀-정사각형 어쩌구",
+                price = 10000
+            )
+        },
+    )
+}
