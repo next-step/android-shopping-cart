c581e4cf69edd1934f16f654d723c40f0aafa9eb
hyemdooly <hyemdooly@gmail.com>
2025-03-08 19:56:52 +0900

    refactor: string 리소스 분리

--- app/src/main/java/nextstep/shoppingcart/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ProductListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import nextstep.shoppingcart.component.ProductList
@@ -21,7 +22,7 @@ fun ProductListScreen(
 ) {
     Scaffold(
         topBar = {
-            ProductListTopAppBar(title = "상품 목록")
+            ProductListTopAppBar(title = stringResource(R.string.product_list))
         }
     ) { innerPadding ->
         ProductList(
--- app/src/main/java/nextstep/shoppingcart/component/ProductListTopAppBar.kt
+++ app/src/main/java/nextstep/shoppingcart/component/ProductListTopAppBar.kt
@@ -12,8 +12,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import nextstep.shoppingcart.R
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -34,7 +36,7 @@ fun ProductListTopAppBar(
         actions = {
             Icon(
                 imageVector = Icons.Filled.ShoppingCart,
-                contentDescription = "장바구니",
+                contentDescription = stringResource(R.string.cart),
                 modifier = Modifier.padding(12.dp),
                 tint = Color.Black
             )
