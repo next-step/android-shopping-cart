9ceb412ab37a6d8a8134f82e8a977e4bbc58efc5
YoonJam <yhw1904@gmail.com>
2025-03-09 00:06:39 +0900

    test: topAppBar 장바구니 개수 테스트 추가

--- app/src/androidTest/java/nextstep/shoppingcart/productList/ProductListTopAppBarTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/productList/ProductListTopAppBarTest.kt
@@ -1,6 +1,9 @@
 package nextstep.shoppingcart.productList
 
+import androidx.compose.ui.test.isDisplayed
+import androidx.compose.ui.test.isNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import org.junit.Rule
 import org.junit.Test
@@ -22,6 +25,43 @@ class ProductListTopAppBarTest {
         // then
         composeTestRule
             .onNodeWithText("상품 목록")
+            .assertExists()
+    }
+
+    @Test
+    fun ProductListTopAppBar의_count인자가_0이면_뱃지가_0개여야_한다() {
+        // given
+        composeTestRule.setContent {
+            ProductListTopAppBar(
+                count = 0,
+                onCartButtonClick = {},
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithTag("cart_badge")
+            .isNotDisplayed()
+    }
+
+    @Test
+    fun ProductListTopAppBar의_count인자가_3이면_뱃지가_3개여야_한다() {
+        // given
+        composeTestRule.setContent {
+            ProductListTopAppBar(
+                count = 3,
+                onCartButtonClick = {},
+            )
+        }
+
+        // then
+        composeTestRule
+            .onNodeWithTag("cart_badge")
+            .isDisplayed()
+
+        // then
+        composeTestRule
+            .onNodeWithText("3")
             .assertExists()
     }
 }
--- app/src/main/java/nextstep/shoppingcart/productList/ProductListTopAppBar.kt
+++ app/src/main/java/nextstep/shoppingcart/productList/ProductListTopAppBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 
 
@@ -39,6 +40,7 @@ fun ProductListTopAppBar(
                     badge = {
                         if (count > 0) {
                             Badge(
+                                modifier = Modifier.testTag("cart_badge"),
                                 containerColor = Color.Red,
                                 contentColor = Color.White
                             ) {
