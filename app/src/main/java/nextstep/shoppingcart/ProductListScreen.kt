feab95f9c6fca600734f0fd6f4d1059c30f0c4db
Song Hyemin <hyemdooly@gmail.com>
2025-03-08 11:04:43 +0900

    🚀 1단계 - 장바구니(상품 목록) (#128)
    * feat: 상품 목록 탑 앱바 구현
    
    * chore: Coil implementation 추가
    
    * feat: Product 클래스 구현
    
    * feat: 리스트 아이템 뷰 구현
    
    * feat: 리스트 뷰 구현
    
    * feat: ProductListScreen 구현

---
+++ app/src/main/java/nextstep/shoppingcart/ProductListScreen.kt
@@ -0,0 +1,55 @@
+package nextstep.shoppingcart
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import nextstep.shoppingcart.component.ProductList
+import nextstep.shoppingcart.component.ProductListTopAppBar
+import nextstep.shoppingcart.model.Product
+
+@Composable
+fun ProductListScreen(
+    products: List<Product>,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        topBar = {
+            ProductListTopAppBar(
+                title = "상품 목록",
+                actions = {
+                    Icon(
+                        imageVector = Icons.Filled.ShoppingCart,
+                        contentDescription = "장바구니",
+                        modifier = Modifier.padding(12.dp),
+                    )
+                }
+            )
+        }
+    ) { innerPadding ->
+        ProductList(
+            modifier = modifier.padding(innerPadding),
+            products = products
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListScreenPreview() {
+    ProductListScreen(
+        products = List(10) {
+            Product(
+                id = it,
+                imageUrl = "https://picsum.photos/id/1/300/300",
+                name = "PET보틀-정사각형 어쩌구",
+                price = 10000
+            )
+        }
+    )
+}
---
+++ app/src/main/java/nextstep/shoppingcart/component/ProductListItem.kt
@@ -0,0 +1,83 @@
+package nextstep.shoppingcart.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import nextstep.shoppingcart.R
+import nextstep.shoppingcart.model.Product
+
+@Composable
+fun ProductListItem(
+    product: Product,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+    ) {
+        AsyncImage(
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f),
+            model = product.imageUrl,
+            contentDescription = product.name,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 23.dp),
+            text = product.name,
+            style = TextStyle(
+                fontWeight = FontWeight.W700,
+                fontSize = 16.sp,
+                lineHeight = 14.sp,
+                letterSpacing = 0.5.sp
+            ),
+            color = Color(0xFF333333),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = Modifier.height(2.dp))
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 4.dp, end = 86.dp),
+            text = stringResource(R.string.price_format, product.price),
+            style = TextStyle(
+                fontWeight = FontWeight.W400,
+                fontSize = 16.sp,
+                lineHeight = 20.sp,
+                letterSpacing = 0.5.sp
+            ),
+            color = Color(0xFF333333)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductListItemPreview() {
+    ProductListItem(
+        product = Product(
+            id = 0,
+            imageUrl = "https://picsum.photos/id/1/300/300",
+            name = "상품명",
+            price = 10000
+        )
+    )
+}
