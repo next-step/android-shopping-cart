42f13a0d8c9f0449090a857397402535d2f59d4e
YoonJam <yhw1904@gmail.com>
2025-03-09 12:41:25 +0900

    refactor: 상태를 갖는 cartScreen 컴포저블 함수 오버로딩

--- app/src/main/java/nextstep/shoppingcart/cart/CartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/cart/CartScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +23,62 @@ import nextstep.shoppingcart.component.CtaButton
 import nextstep.shoppingcart.data.Cart
 import nextstep.shoppingcart.data.CartItem
 import nextstep.shoppingcart.data.DummyProduct
+
+@Composable
+fun CartScreen(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            CartTopAppBar(onBackButtonClick = {
+                if (context is CartActivity) {
+                    context.finish()
+                }
+            })
+        },
+        containerColor = Color.White,
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding),
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxSize(),
+                contentPadding = PaddingValues(horizontal = 18.dp, vertical = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                items(Cart.items) {
+                    CartProduct(
+                        cartItem = it,
+                        onDeleteButtonClick = { cartItem ->
+                            Cart.removeAll(cartItem.product)
+                        },
+                        onMinusButtonClick = { cartItem ->
+                            Cart.removeOne(cartItem.product)
+                        },
+                        onPlusButtonClick = { cartItem ->
+                            Cart.addOne(cartItem.product)
+                        },
+                    )
+                }
+            }
+
+            CtaButton(
+                text = stringResource(R.string.order, Cart.totalPrice),
+                onClick = { /* TODO 주문하기 */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(54.dp)
+                    .testTag("orderButton"),
+            )
+        }
+    }
+}
+
 
 @Composable
 fun CartScreen(
@@ -75,13 +132,17 @@ fun CartScreen(
 @Preview(showBackground = true)
 @Composable
 private fun CartScreenPreview() {
-    Cart.addOne(DummyProduct.product1)
-    Cart.addOne(DummyProduct.product2)
-    Cart.addOne(DummyProduct.product3)
+    val cartItems = listOf(
+        CartItem(DummyProduct.product1, 1),
+        CartItem(DummyProduct.product2, 2),
+        CartItem(DummyProduct.product3, 3),
+    )
+
+    val totalPrice = cartItems.sumOf { it.product.price * it.count }
 
     CartScreen(
-        cartItems = Cart.items,
-        totalPrice = Cart.totalPrice,
+        cartItems = cartItems,
+        totalPrice = totalPrice,
         onBackButtonClick = {},
         onDeleteButtonClick = {},
         onMinusButtonClick = {},
