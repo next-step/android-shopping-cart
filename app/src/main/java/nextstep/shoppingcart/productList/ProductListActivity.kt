e2ec4b8f83732012949e8f158ebef6d5c761fcb4
YoonJam <yhw1904@gmail.com>
2025-03-10 10:21:36 +0900

    refactor: productList 상태 있는 것과 없는 것으로 나누기

--- app/src/main/java/nextstep/shoppingcart/productList/ProductListActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/productList/ProductListActivity.kt
@@ -3,15 +3,6 @@ package nextstep.shoppingcart.productList
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import androidx.compose.runtime.toMutableStateList
-import androidx.compose.ui.tooling.preview.Preview
-import nextstep.shoppingcart.cart.CartActivity
-import nextstep.shoppingcart.data.Cart
-import nextstep.shoppingcart.data.DummyProduct
-import nextstep.shoppingcart.data.Product
-import nextstep.shoppingcart.productDetail.ProductDetailActivity
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class ProductListActivity : ComponentActivity() {
@@ -19,50 +10,8 @@ class ProductListActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingCartTheme {
-                val productToCount = DummyProduct.productDummyList.map {
-                    it to (Cart.getCartCount(it))
-                }
-
-                val products: SnapshotStateList<Pair<Product, Int>> =
-                    productToCount.toMutableStateList()
-                val totalCount = products.sumOf { it.second }
-
-                ProductListScreen(
-                    productAndCountList = products,
-                    totalCount = totalCount,
-                    onItemClick = { product ->
-                        ProductDetailActivity.start(this, product)
-                    },
-                    onCartButtonClick = {
-                        CartActivity.start(this)
-                    },
-                    onPlushClick = { product ->
-                        Cart.addOne(product)
-                    },
-                    onMinusClick = { product ->
-                        Cart.removeOne(product)
-                    },
-                )
+                ProductListScreen()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun ProductListActivityPreview() {
-    val productAndCountList = DummyProduct.productDummyList.map {
-        it to (Cart.getCartCount(it))
-    }
-
-    ShoppingCartTheme {
-        ProductListScreen(
-            productAndCountList = productAndCountList,
-            totalCount = 0,
-            onItemClick = {},
-            onCartButtonClick = {},
-            onPlushClick = {},
-            onMinusClick = {},
-        )
     }
 }
--- app/src/main/java/nextstep/shoppingcart/productList/ProductListScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/productList/ProductListScreen.kt
@@ -3,12 +3,57 @@ package nextstep.shoppingcart.productList
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import nextstep.shoppingcart.cart.CartActivity
 import nextstep.shoppingcart.data.Cart
 import nextstep.shoppingcart.data.DummyProduct
 import nextstep.shoppingcart.data.Product
+import nextstep.shoppingcart.productDetail.ProductDetailActivity
+
+@Composable
+fun ProductListScreen(
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+
+    val productToCount = DummyProduct.productDummyList.map {
+        it to (Cart.getCartCount(it))
+    }
+
+    val products: SnapshotStateList<Pair<Product, Int>> = productToCount.toMutableStateList()
+    val totalCount = products.sumOf { it.second }
+
+    val onItemClick: (Product) -> Unit = { product ->
+        ProductDetailActivity.start(context, product)
+    }
+
+    val onCartButtonClick: () -> Unit = {
+        CartActivity.start(context)
+    }
+
+    val onPlushClick: (Product) -> Unit = { product ->
+        Cart.addOne(product)
+    }
+
+    val onMinusClick: (Product) -> Unit = { product ->
+        Cart.removeOne(product)
+    }
+
+    ProductListScreen(
+        productAndCountList = products,
+        totalCount = totalCount,
+        onItemClick = onItemClick,
+        onCartButtonClick = onCartButtonClick,
+        onPlushClick = onPlushClick,
+        onMinusClick = onMinusClick,
+        modifier = modifier,
+    )
+}
 
 @Composable
 fun ProductListScreen(
