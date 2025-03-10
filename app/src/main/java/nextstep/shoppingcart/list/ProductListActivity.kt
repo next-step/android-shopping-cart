550c5bd33878a8ede38e1423f47aafab89fccc75
hyemdooly <hyemdooly@gmail.com>
2025-03-11 00:35:32 +0900

    feat: 상품 목록 화면에 컴포넌트 추가 및 로직 구현

--- app/src/main/java/nextstep/shoppingcart/list/ProductListActivity.kt
+++ app/src/main/java/nextstep/shoppingcart/list/ProductListActivity.kt
@@ -1,9 +1,16 @@
 package nextstep.shoppingcart.list
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.remember
+import nextstep.shoppingcart.Cart
+import nextstep.shoppingcart.cart.CartActivity
 import nextstep.shoppingcart.common.Products
+import nextstep.shoppingcart.detail.ProductDetailActivity
+import nextstep.shoppingcart.list.model.ProductWithCount
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
 
 class ProductListActivity : ComponentActivity() {
@@ -11,8 +18,32 @@ class ProductListActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ShoppingCartTheme {
+                val items = remember {
+                    derivedStateOf {
+                        Products.items.map { product ->
+                            ProductWithCount(
+                                product,
+                                Cart.items.firstOrNull { it.product.id == product.id }?.count ?: 0
+                            )
+                        }
+                    }
+                }
+
                 ProductListScreen(
-                    products = Products.items
+                    products = items.value,
+                    onClickProduct = {
+                        startActivity(ProductDetailActivity.intent(this, it))
+                    },
+                    onClickCart = {
+                        startActivity(CartActivity.intent(this))
+                    },
+                    onChangeCount = { id, count ->
+                        if (count == 1) {
+                            val item = Products.items.find { it.id == id }
+                            if (item != null) Cart.addOne(item)
+                        }
+                        Cart.changeCount(id, count)
+                    }
                 )
             }
         }
