104a1e9f77f4defb936bf06e3f2287b9db65c185
KimMooHyeon <dlfb77@gmail.com>
2025-03-16 22:13:20 +0900

    refactor : CrossButton 컴포져블modifier 외부 노출

--- app/src/main/java/nextstep/shoppingcart/ui/component/CrossButton.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/component/CrossButton.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
@@ -18,21 +19,23 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun CrossButton(onClick: () -> Unit) {
-    Box(
-        modifier = Modifier
-            .size(42.dp)
-            .clip(CircleShape)
-            .border(1.dp, Color.Black, CircleShape)
-            .background(Color.White)
-            .clickable(onClick = onClick),
-        contentAlignment = Alignment.Center
-    ) {
-        Icon(
-            Icons.Filled.Add,
-            contentDescription = "Add",
-            tint = Color.Black
-        )
+fun CrossButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.BottomEnd) {
+        Box(
+            modifier = Modifier
+                .size(42.dp)
+                .clip(CircleShape)
+                .border(1.dp, Color.Black, CircleShape)
+                .background(Color.White)
+                .clickable(onClick = onClick),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                contentDescription = "Add",
+                tint = Color.Black
+            )
+        }
     }
 }
 
