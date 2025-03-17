355dd0a43e1c4b9ff737e9a4fe3971954c303e4a
김무현 <2230198@ad.h-firework.com>
2025-03-17 16:03:25 +0900

    test: 상품리스트 각 테스트 케이스별 테스트 가독성 및 명확성을 위한 uiState 각 함수별 설정

--- app/src/androidTest/java/nextstep/shoppingcart/ProductListScreenTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/ProductListScreenTest.kt
@@ -1,7 +1,5 @@
 package nextstep.shoppingcart
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
@@ -9,7 +7,6 @@ import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.productlist.ProductListScreen
 import nextstep.shoppingcart.productlist.model.ProductListUiState
 import nextstep.shoppingcart.productlist.model.ProductWithCartInfo
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,14 +14,16 @@ class ProductListScreenTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
-    private val fakeUiState: MutableState<ProductListUiState> =
-        mutableStateOf(ProductListUiState.Loading)
 
-    @Before
-    fun setup() {
+    @Test
+    fun 상품_리스트가_없을_경우에는_텅_글자가_노출된다() {
+        // given
+        val uiState = ProductListUiState.Empty
+
+        // when
         composeTestRule.setContent {
             ProductListScreen(
-                uiState = fakeUiState.value,
+                uiState = uiState,
                 navigateToProductDetail = {},
                 navigateToCart = {},
                 onDecreaseProductQuantity = {},
@@ -32,12 +31,6 @@ class ProductListScreenTest {
                 modifier = Modifier
             )
         }
-    }
-
-    @Test
-    fun 상품_리스트가_없을_경우에는_텅_글자가_노출된다() {
-        // when
-        fakeUiState.value = ProductListUiState.Empty
 
         // then
         composeTestRule
@@ -47,8 +40,20 @@ class ProductListScreenTest {
 
     @Test
     fun 상품_리스트_로드_실패시_다시시도_버튼이_노출된다() {
+        // given
+        val uiState = ProductListUiState.Error
+
         // when
-        fakeUiState.value = ProductListUiState.Error
+        composeTestRule.setContent {
+            ProductListScreen(
+                uiState = uiState,
+                navigateToProductDetail = {},
+                navigateToCart = {},
+                onDecreaseProductQuantity = {},
+                onIncreaseProductQuantity = {},
+                modifier = Modifier
+            )
+        }
 
         // then
         composeTestRule
@@ -58,20 +63,33 @@ class ProductListScreenTest {
 
     @Test
     fun 상품_리스트_로드_성공시_상품이_노출된다() {
+        // given
+        val uiState = ProductListUiState.Success(
+            products = productTestDataList.map { ProductWithCartInfo(it) }
+        )
+
         // when
-        fakeUiState.value =
-            ProductListUiState.Success(products = productTestDataList.map { ProductWithCartInfo(it) })
+        composeTestRule.setContent {
+            ProductListScreen(
+                uiState = uiState,
+                navigateToProductDetail = {},
+                navigateToCart = {},
+                onDecreaseProductQuantity = {},
+                onIncreaseProductQuantity = {},
+                modifier = Modifier
+            )
+        }
 
         // then
         composeTestRule
-            .onNodeWithText("상품 목록")
+            .onNodeWithText("테스트0")
             .assertExists()
     }
 
     companion object {
         private val productTestDataList = List(30) { i ->
             Product(
-                name = "테스트",
+                name = "테스트$i",
                 imageUrl = "https://i.namu.wiki/i/rwoGbf-OhaV1A1I77FtQEWojKsa-i9J0HZ0E3tFfr4gdi7fCHRh7DwaqLkLzKdruftxpu_twLfkhwgMxc3QrvgY9HhwbwB7W_YPGbkjpCIxFO9abcyQSLgM8NVUkKJ6WPegKkT35ukb0NXXRHeMW1zGcxZz_9zx63o9Pnat6I3Q.webp",
                 price = 10000 + i,
                 productId = "id${i}"
