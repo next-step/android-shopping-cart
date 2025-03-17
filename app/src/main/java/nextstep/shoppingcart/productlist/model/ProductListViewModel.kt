01a6b09e92055b04588d881a75348a07e60008e4
김무현 <2230198@ad.h-firework.com>
2025-03-17 14:58:09 +0900

    feat: 장바구니 데이터 변경을 감지하여 상품 리스트 표출 UI 업데이트 로직추가

--- app/src/main/java/nextstep/shoppingcart/productlist/model/ProductListViewModel.kt
+++ app/src/main/java/nextstep/shoppingcart/productlist/model/ProductListViewModel.kt
@@ -8,17 +8,36 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import nextstep.shoppingcart.data.Cart
+import nextstep.shoppingcart.data.ProductsTestData
 import nextstep.shoppingcart.model.Product
 
 class ProductListViewModel(private val repository: Cart = Cart) :
     ViewModel() {
 
-    private val _uiState = MutableStateFlow(ProductListUiState.Loading)
+    private val _uiState: MutableStateFlow<ProductListUiState> =
+        MutableStateFlow(ProductListUiState.Loading)
     val uiState: StateFlow<ProductListUiState> = _uiState.asStateFlow()
 
     init {
+        _uiState.value = ProductListUiState.Success(ProductsTestData.productTestDataList.map {
+            ProductWithCartInfo(it)
+        })
         viewModelScope.launch {
             repository.itemsFlow.collectLatest { cartItems ->
+                val newMap = mutableMapOf<String, Int>()
+                cartItems.forEach {
+                    newMap[it.product.productId] = it.count
+                }
+                val currentUiState = _uiState.value
+                if (currentUiState is ProductListUiState.Success) {
+                    _uiState.value = currentUiState.copy(
+                        products = currentUiState.products.map { productWithCartInfo ->
+                            productWithCartInfo.copy(
+                                cartCount = newMap[productWithCartInfo.product.productId] ?: 0
+                            )
+                        }
+                    )
+                }
             }
         }
     }
@@ -29,9 +48,5 @@ class ProductListViewModel(private val repository: Cart = Cart) :
 
     fun removeOne(product: Product) {
         repository.removeOne(product)
-    }
-
-    fun removeAll(product: Product) {
-        repository.removeAll(product)
     }
 }
