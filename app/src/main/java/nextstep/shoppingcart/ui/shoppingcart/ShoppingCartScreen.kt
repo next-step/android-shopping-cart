9cfff1a6363524ef5e1870b1f969fea35a3f5c83
차민지 <ckalswl331@naver.com>
2025-03-30 15:38:18 +0900

    refactor: 3단계 리뷰 사항 업데이트
    change
    - 불필요한 derivedStateOf 제거
    - ShoppingCartScreen을 Stateless한 Screen과 Stateful한 screen으로 나누기

--- app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
+++ app/src/main/java/nextstep/shoppingcart/ui/shoppingcart/ShoppingCartScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -15,6 +15,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import nextstep.shoppingcart.R
 import nextstep.shoppingcart.data.Cart
+import nextstep.shoppingcart.model.CartItem
 import nextstep.shoppingcart.model.Product
 import nextstep.shoppingcart.ui.BottomButton
 import nextstep.shoppingcart.ui.theme.ShoppingCartTheme
@@ -29,8 +30,29 @@ fun ShoppingCartScreen(
     onDelete: (Product) -> Unit = {},
 ) {
     val products = Cart.items
-    val totalPrice by remember { derivedStateOf { Cart.totalPrice } }
+    val totalPrice by remember { mutableStateOf(Cart.totalPrice) }
 
+    ShoppingCardScreen(
+        onBackClick = onBackClick,
+        totalPrice = totalPrice,
+        products = products,
+        onItemAdd = onItemAdd,
+        onItemRemove = onItemRemove,
+        onDelete = onDelete,
+        modifier = modifier
+    )
+}
+
+@Composable
+internal fun ShoppingCardScreen(
+    onBackClick: () -> Unit,
+    totalPrice: Int,
+    products: List<CartItem>,
+    onItemAdd: (Product) -> Unit,
+    onItemRemove: (Product) -> Unit,
+    onDelete: (Product) -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
         topBar = {
@@ -65,6 +87,13 @@ fun ShoppingCartScreen(
 @Composable
 private fun ShoppingCartScreenPreview() {
     ShoppingCartTheme {
-        ShoppingCartScreen()
+        ShoppingCardScreen(
+            onBackClick = {},
+            totalPrice = 10000,
+            products = listOf(CartItem(Product("", "Product", 10000), 1)),
+            onItemAdd = {},
+            onItemRemove = {},
+            onDelete = {}
+        )
     }
 }
