2f3f74951e9e988ece1787c8b96b69963ea8a84e
차민지 <ckalswl331@naver.com>
2025-04-06 23:13:34 +0900

    refactor: CartScreenTest를 UI 상태 및 동작 검증 중심으로 변경
    change
     - 비즈니스 로직 제거

--- app/src/androidTest/java/nextstep/shoppingcart/CartScreenTest.kt
+++ app/src/androidTest/java/nextstep/shoppingcart/CartScreenTest.kt
@@ -25,39 +25,15 @@ class CartScreenTest {
     var totalPrice by mutableIntStateOf(0)
         private set
 
-    private var onItemAdd: (Product) -> Unit = { product ->
-        val cartItem = cartItems.find { it.product == product }
-        if (cartItem == null) {
-            cartItems.add(CartItem(product, 1))
-        } else {
-            val index = cartItems.indexOf(cartItem)
-            cartItems[index] = cartItem.copy(count = cartItem.count + 1)
-        }
-        totalPrice += product.price
-    }
-    private var onItemRemove: (Product) -> Unit = { product ->
-        if (cartItems.filter { it.product == product }.size > 0) {
-            cartItems.remove(cartItems.filter { it.product == product }[0])
-            totalPrice -= product.price
-        }
-    }
-    private var onDelete: (Product) -> Unit = { product ->
-        val cartItem = cartItems.find { it.product == product }
-        if (cartItem != null) {
-            cartItems.removeIf { it.product == product }
-            totalPrice -= cartItem.product.price * cartItem.count
-        }
-    }
-
     @Before
     fun setup() {
         composeTestRule.setContent {
             ShoppingCartScreen(
                 products = cartItems,
                 totalPrice = totalPrice,
-                onItemAdd = onItemAdd,
-                onItemRemove = onItemRemove,
-                onDelete = onDelete,
+                onItemAdd = {},
+                onItemRemove = {},
+                onDelete = {},
                 onBackClick = {}
             )
         }
@@ -66,42 +42,27 @@ class CartScreenTest {
     @Test
     fun 담긴_상품_가격의_총합이_노출된다() {
         // given: 1000원, 2000원, 3000원 상품이 장바구니에 추가되었을 때
-        onItemAdd(Product("", "상품1", 1000))
-        onItemAdd(Product("", "상품2", 2000))
-        onItemAdd(Product("", "상품3", 3000))
+        cartItems.add(CartItem(Product("", "상품1", 1000), 1))
+        cartItems.add(CartItem(Product("", "상품2", 2000), 1))
+        cartItems.add(CartItem(Product("", "상품3", 3000), 1))
+        totalPrice = 6000
 
         // when
         composeTestRule.waitForIdle()
         // then
-        composeTestRule.onNodeWithText("6,000원", substring = true).assertExists()
-    }
-
-    @Test
-    fun 담긴_상품을_제거할_수_있다() {
-        // given: 1000원, 2000원, 3000원 상품이 장바구니에 있을 때
-        onItemAdd(Product("", "상품1", 1000))
-        onItemAdd(Product("", "상품2", 2000))
-        onItemAdd(Product("", "상품3", 3000))
-        composeTestRule.waitForIdle()
-
-        // when: 상품2를 장바구니에서 제거
-        composeTestRule.onAllNodesWithContentDescription("삭제")[1].performClick()
-        composeTestRule.waitForIdle()
-
-        // then: 상품2가 없어야 함
-        composeTestRule
-            .onNodeWithText("상품2")
-            .assertDoesNotExist()
+        composeTestRule.onNodeWithText("주문하기(6,000원)", substring = true).assertExists()
     }
 
     @Test
     fun 담긴_상품의_수량을_증가시키면_상품_가격에_반영된다() {
         // given: 가격이 1000원인 상품이 1개 있을 때
-        onItemAdd(Product("", "상품1", 1000))
+        cartItems.add(CartItem(Product("", "상품1", 1000), 1))
+        totalPrice = 1000
         composeTestRule.waitForIdle()
 
         // when: 상품의 수량을 2개로 증가
-        composeTestRule.onAllNodesWithContentDescription("추가")[0].performClick()
+        cartItems[0] = CartItem(cartItems[0].product, 2)
+        totalPrice = 2000
         composeTestRule.waitForIdle()
 
         //then: 상품 가격 총합이 2000원이 되어야 한다.
@@ -111,26 +72,27 @@ class CartScreenTest {
     @Test
     fun 담긴_상품의_수량을_감소시키면_상품_가격에_반영된다() {
         // given: 가격이 1000원인 상품이 3개 있을 때
-        onItemAdd(Product("", "상품1", 1000))
-        onItemAdd(Product("", "상품1", 1000))
+        cartItems.add(CartItem(Product("", "상품1", 1000), 3))
+        totalPrice = 3000
         composeTestRule.waitForIdle()
 
-        // when: 상품의 수량을 2개로 증가
-        composeTestRule.onAllNodesWithContentDescription("빼기")[0].performClick()
+        // when: 상품의 수량을 2개로 감소
+        cartItems[0] = CartItem(cartItems[0].product, 2)
+        totalPrice = 2000
         composeTestRule.waitForIdle()
 
         //then: 상품 가격 총합이 2000원이 되어야 한다.
-        composeTestRule.onNodeWithText("1,000원", substring = true).assertExists()
+        composeTestRule.onNodeWithText("2,000원", substring = true).assertExists()
     }
 
     @Test
-    fun 담긴_상품의_수량을_1보다_적게_하면_상품이_삭제된다() {
+    fun 담긴_상품을_제거하면_빈_화면이_노출된다() {
         // given: 가격이 1000원인 상품이 1개 있을 때
-        onItemAdd(Product("", "상품1", 1000))
+        cartItems.add(CartItem(Product("", "상품1", 1000), 3))
         composeTestRule.waitForIdle()
 
         // when: 상품의 수량을 감소
-        composeTestRule.onAllNodesWithContentDescription("빼기")[0].performClick()
+        cartItems.clear()
         composeTestRule.waitForIdle()
 
         // then: 상품1이 없어야 함
